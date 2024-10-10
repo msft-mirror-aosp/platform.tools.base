cfd44d05c1c573950d9aa62bd89956400115a43a
Pierfrancesco Soffritti <psoffritti@google.com>
2024-10-10 14:24:40 +0100

    Ignore regular android views if xr views are present
    If there are xr panels, we want to ignore regular android views.
    In practice, there are no regular views in an xr app,
    but the regular view APIs still return the main panel views as
    a regular view. So getting views both using xr and regular view
    APIs would duplicate the main panel.
    
    Test: NA
    Bug: NA
    Change-Id: If4636538a3e2ac745136c8913bda3da4f5e7ceb2

--- dynamic-layout-inspector/agent/appinspection/src/main/com/android/tools/agent/appinspection/RootsDetector.kt
+++ dynamic-layout-inspector/agent/appinspection/src/main/com/android/tools/agent/appinspection/RootsDetector.kt
@@ -143,7 +143,16 @@ data class InspectorView(val view: View, val isXr: Boolean)
 
 fun getRootViews(xrHelper: XrHelper): List<InspectorView> {
     val xrViews = xrHelper.getXrViews().map { InspectorView(it, true) }
-    val androidViews = getAndroidViews().map { InspectorView(it, false) }
+
+    // If there are xr panels, we want to ignore regular android views.
+    // In practice, there are no regular views in an xr app, but the regular view APIs
+    // still return the main panel views as a regular view. So getting views both using
+    // xr and regular view APIs would duplicate the main panel.
+    val androidViews = if (xrViews.isEmpty()) {
+      getAndroidViews().map { InspectorView(it, false) }
+    } else {
+      emptyList()
+    }
 
     return (xrViews + androidViews)
         .filter { it.view.visibility == View.VISIBLE && it.view.isAttachedToWindow }
