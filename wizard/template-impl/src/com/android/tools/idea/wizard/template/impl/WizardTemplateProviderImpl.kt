d116ca3f1a4fe20f96a10b17cceb067f6cd76608
Alex Vanyo <vanyo@google.com>
2025-01-10 00:16:30 +0000

    Add Compose Navigation UI template to wizard
    Adds a new template with NavigationSuiteScaffold, based on the existing
    Compose empty Activity template with Material 3.
    
    Bug: 359910572
    Test: Added TemplateDiffTest in linked CL
    
    Change-Id: I80d42a2544efd124df826375c08e068868e85286

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/WizardTemplateProviderImpl.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/WizardTemplateProviderImpl.kt
@@ -22,6 +22,7 @@ import com.android.tools.idea.wizard.template.impl.activities.androidTVActivity.
 import com.android.tools.idea.wizard.template.impl.activities.basicActivity.basicActivityTemplate
 import com.android.tools.idea.wizard.template.impl.activities.bottomNavigationActivity.bottomNavigationActivityTemplate
 import com.android.tools.idea.wizard.template.impl.activities.composeActivityMaterial3.composeActivityMaterial3Template
+import com.android.tools.idea.wizard.template.impl.activities.composeNavigationUiActivityMaterial3.composeNavigationUiActivityMaterial3Template
 import com.android.tools.idea.wizard.template.impl.activities.composeTvActivity.composeTvActivityTemplate
 import com.android.tools.idea.wizard.template.impl.activities.composeWearActivity.composeWearActivityTemplate
 import com.android.tools.idea.wizard.template.impl.activities.composeWearActivity.composeWearActivityWithTileAndComplicationTemplate
@@ -76,6 +77,7 @@ class WizardTemplateProviderImpl : WizardTemplateProvider() {
   override fun getTemplates(): List<Template> = listOf(
       composeActivityMaterial3Template,
       genAiActivityTemplate,
+      composeNavigationUiActivityMaterial3Template,
       xrActivityTemplate,
     androidTVActivityTemplate,
     basicActivityTemplate,
---
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivityMaterial3Navigation/src/app_package/mainActivityKt.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.idea.wizard.template.impl.activities.composeNavigationUiActivityMaterial3.src.app_package
+
+import com.android.tools.idea.wizard.template.escapeKotlinIdentifier
+
+fun mainActivityKt(
+  activityClass: String,
+  defaultPreview: String,
+  greeting: String,
+  packageName: String,
+  themeName: String,
+  appComposableName: String,
+) = """
+package ${escapeKotlinIdentifier(packageName)}
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import ${escapeKotlinIdentifier(packageName)}.ui.theme.${themeName}
+
+class $activityClass : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            $themeName {
+                $appComposableName()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun $appComposableName() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            it.icon,
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            ${greeting}(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: ImageVector,
+) {
+    HOME("Home", Icons.Default.Home),
+    FAVORITES("Favorites", Icons.Default.Favorite),
+    PROFILE("Profile", Icons.Default.AccountBox),
+}
+
+@Composable
+fun ${greeting}(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello ${"$"}name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ${defaultPreview}() {
+    $themeName {
+        ${greeting}("Android")
+    }
+}
+"""
