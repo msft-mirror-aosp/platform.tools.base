f7656a6d30cd82ad2e89e98afee9949964f1c557
Phil Nguyen <philnguyen@google.com>
2025-02-25 16:14:09 -0800

    Move existing generic utils one package down
    Bug: n/a
    Test: existing, moved
    Change-Id: I3844f57d53441b4f4955de685e99d0fe29793d54

--- lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/fx/utils/PersistentCollectionUtils.kt
+++ lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/fx/utils/PersistentCollectionUtils.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.lint.checks.fx
+package com.android.tools.lint.checks.fx.utils
 
 import kotlinx.collections.immutable.PersistentMap
 import kotlinx.collections.immutable.PersistentSet
