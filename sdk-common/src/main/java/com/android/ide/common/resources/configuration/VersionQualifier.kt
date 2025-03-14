cfffebf02ade04c4b8feb765146af6f2d5befac1
Andy Magee <andymagee@google.com>
2025-03-14 10:05:38 -0700

    Convert VersionQualifier to kt (2/3)
    Automated IDE conversion + ktfmt
    
    Bug: 391945620
    Test: VersionQualifierTest
    Change-Id: I705707ac0f1a6e6888476c720db27570d71c9e51

--- sdk-common/src/main/java/com/android/ide/common/resources/configuration/VersionQualifier.kt
+++ sdk-common/src/main/java/com/android/ide/common/resources/configuration/VersionQualifier.kt
@@ -13,166 +13,148 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.ide.common.resources.configuration;
+package com.android.ide.common.resources.configuration
 
-import com.android.annotations.NonNull;
-import com.android.annotations.Nullable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
-/** Resource qualifier for Platform version. */
-public final class VersionQualifier extends ResourceQualifier {
-    /** Default version. This means the property is not set. */
-    public static final int DEFAULT_VERSION = -1;
+/** Resource qualifier for Platform version.  */
+class VersionQualifier : ResourceQualifier {
+  var version: Int = DEFAULT_VERSION
+    private set
 
-    private static final Pattern sVersionPattern = Pattern.compile("^v(\\d+)$");
+  constructor(apiLevel: Int) {
+    this.version = apiLevel
+  }
 
-    private int mVersion = DEFAULT_VERSION;
+  constructor()
 
-    public static final String NAME = "Platform Version";
+  override fun getName(): String {
+    return NAME
+  }
+
+  override fun getShortName(): String {
+    return "Version"
+  }
+
+  override fun since(): Int {
+    return 1
+  }
+
+  override fun isValid(): Boolean {
+    return this.version != DEFAULT_VERSION
+  }
+
+  override fun hasFakeValue(): Boolean {
+    return false
+  }
+
+  override fun checkAndSet(value: String, config: FolderConfiguration): Boolean {
+    val qualifier: VersionQualifier? = getQualifier(value)
+    if (qualifier != null) {
+      config.setVersionQualifier(qualifier)
+      return true
+    }
+
+    return false
+  }
+
+  override fun equals(qualifier: Any?): Boolean {
+    return qualifier is VersionQualifier
+      && this.version == qualifier.version
+  }
+
+  override fun isMatchFor(qualifier: ResourceQualifier): Boolean {
+    if (qualifier is VersionQualifier) {
+      // It is considered a match if our API level is equal or lower to the given qualifier,
+      // or the given qualifier doesn't specify an API Level.
+      return this.version <= qualifier.version
+        || qualifier.version == DEFAULT_VERSION
+    }
+
+    return false
+  }
+
+  override fun isBetterMatchThan(
+    compareTo: ResourceQualifier?, reference: ResourceQualifier
+  ): Boolean {
+    if (compareTo == null) {
+      return true
+    }
+
+    val compareQ = compareTo as VersionQualifier
+    val referenceQ = reference as VersionQualifier
+
+    if (compareQ.version == referenceQ.version) {
+      // what we have is already the best possible match (exact match)
+      return false
+    } else if (this.version == referenceQ.version) {
+      // got new exact value, this is the best!
+      return true
+    } else {
+      // In all case we're going to prefer the higher version (since they have been filtered
+      // to not be too high.)
+      return this.version > compareQ.version
+    }
+  }
+
+  override fun hashCode(): Int {
+    return this.version
+  }
+
+  /**
+   * Returns the string used to represent this qualifier in the folder name.
+   */
+  override fun getFolderSegment(): String {
+    return getFolderSegment(this.version)
+  }
+
+  override fun getShortDisplayValue(): String {
+    return if (this.version == DEFAULT_VERSION) "" else "API " + this.version
+  }
+
+  override fun getLongDisplayValue(): String {
+    return if (this.version == DEFAULT_VERSION) "" else "API Level " + this.version
+  }
+
+  companion object {
+    /** Default version. This means the property is not set.  */
+    val DEFAULT_VERSION: Int = -1
+
+    private val sVersionPattern: Pattern = Pattern.compile("^v(\\d+)$")
+
+    const val NAME: String = "Platform Version"
 
     /**
      * Creates and returns a qualifier from the given folder segment. If the segment is incorrect,
-     * {@code null} is returned.
+     * `null` is returned.
      *
      * @param segment the folder segment from which to create a qualifier
-     * @return a new VersionQualifier object or {@code null}
+     * @return a new VersionQualifier object or `null`
      */
-    public static VersionQualifier getQualifier(@NonNull String segment) {
-        Matcher m = sVersionPattern.matcher(segment);
-        if (m.matches()) {
-            String v = m.group(1);
+    fun getQualifier(segment: String): VersionQualifier? {
+      val m: Matcher = sVersionPattern.matcher(segment)
+      if (m.matches()) {
+        val v = m.group(1)
 
-            try {
-                return new VersionQualifier(Integer.parseInt(v));
-            } catch (NumberFormatException e) {
-                // Not a valid version qualifier segment - return null.
-            }
+        try {
+          return VersionQualifier(v.toInt())
+        } catch (e: NumberFormatException) {
+          // Not a valid version qualifier segment - return null.
         }
+      }
 
-        return null;
+      return null
     }
 
     /**
      * Returns the folder name segment for the given version value. This is equivalent to calling
-     * {@code new VersionQualifier(version).toString()}.
+     * `new VersionQualifier(version).toString()`.
      *
-     * @param version the value of the qualifier, as returned by {@link #getVersion()}.
+     * @param version the value of the qualifier, as returned by [.getVersion].
      */
-    public static String getFolderSegment(int version) {
-        return version == DEFAULT_VERSION ? "" : 'v' + Integer.toString(version);
+    fun getFolderSegment(version: Int): String {
+      return if (version == DEFAULT_VERSION) "" else 'v'.toString() + version.toString()
     }
-
-    public VersionQualifier(int apiLevel) {
-        mVersion = apiLevel;
-    }
-
-    public VersionQualifier() {
-        //pass
-    }
-
-    public int getVersion() {
-        return mVersion;
-    }
-
-    @Override
-    public String getName() {
-        return NAME;
-    }
-
-    @Override
-    public String getShortName() {
-        return "Version";
-    }
-
-    @Override
-    public int since() {
-        return 1;
-    }
-
-    @Override
-    public boolean isValid() {
-        return mVersion != DEFAULT_VERSION;
-    }
-
-    @Override
-    public boolean hasFakeValue() {
-        return false;
-    }
-
-    @Override
-    public boolean checkAndSet(@NonNull String value, @NonNull FolderConfiguration config) {
-        VersionQualifier qualifier = getQualifier(value);
-        if (qualifier != null) {
-            config.setVersionQualifier(qualifier);
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean equals(@Nullable Object qualifier) {
-        return qualifier instanceof VersionQualifier
-                && mVersion == ((VersionQualifier) qualifier).mVersion;
-
-    }
-
-    @Override
-    public boolean isMatchFor(@NonNull ResourceQualifier qualifier) {
-        if (qualifier instanceof VersionQualifier) {
-            // It is considered a match if our API level is equal or lower to the given qualifier,
-            // or the given qualifier doesn't specify an API Level.
-            return mVersion <= ((VersionQualifier) qualifier).mVersion
-                    || ((VersionQualifier) qualifier).mVersion == DEFAULT_VERSION;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean isBetterMatchThan(
-            @Nullable ResourceQualifier compareTo, @NonNull ResourceQualifier reference) {
-        if (compareTo == null) {
-            return true;
-        }
-
-        VersionQualifier compareQ = (VersionQualifier) compareTo;
-        VersionQualifier referenceQ = (VersionQualifier) reference;
-
-        if (compareQ.mVersion == referenceQ.mVersion) {
-            // what we have is already the best possible match (exact match)
-            return false;
-        } else if (mVersion == referenceQ.mVersion) {
-            // got new exact value, this is the best!
-            return true;
-        } else {
-            // In all case we're going to prefer the higher version (since they have been filtered
-            // to not be too high.)
-            return mVersion > compareQ.mVersion;
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return mVersion;
-    }
-
-    /**
-     * Returns the string used to represent this qualifier in the folder name.
-     */
-    @Override
-    public String getFolderSegment() {
-        return getFolderSegment(mVersion);
-    }
-
-    @Override
-    public String getShortDisplayValue() {
-        return mVersion == DEFAULT_VERSION ? "" : "API " + mVersion;
-    }
-
-    @Override
-    public String getLongDisplayValue() {
-        return mVersion == DEFAULT_VERSION ? "" : "API Level " + mVersion;
-    }
+  }
 }
