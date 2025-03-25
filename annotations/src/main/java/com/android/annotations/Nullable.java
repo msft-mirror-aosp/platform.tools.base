a1ab1deb8629662543e2b05b66dfd3304cebba30
Kurt Dresner <kdresner@google.com>
2025-03-25 09:56:55 -0700

    Remove NonnullByDefault annotation
    Bug: 76095348
    Test: Existing
    Change-Id: I8275f733ddf52d17c84a7c362ed5467aba370101

--- annotations/src/main/java/com/android/annotations/Nullable.java
+++ annotations/src/main/java/com/android/annotations/Nullable.java
@@ -29,8 +29,7 @@ import java.lang.annotation.Target;
 /**
  * Denotes that a parameter, field or method return value can be null. <b>Note</b>: this is the
  * default assumption for most Java APIs and the default assumption made by most static code
- * checking tools, so usually you don't need to use this annotation; its primary use is to override
- * a default wider annotation like {@link NonNullByDefault}.
+ * checking tools, so usually you don't need to use this annotation.
  *
  * <p>When decorating a method call parameter, this denotes the parameter can legitimately be null
  * and the method will gracefully deal with it. Typically used on optional parameters.
