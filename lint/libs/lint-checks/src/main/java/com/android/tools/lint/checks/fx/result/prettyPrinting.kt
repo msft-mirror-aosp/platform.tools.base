e1270014eda503bb79efabdbd8162b4a35ec9e27
Phil Nguyen <philnguyen@google.com>
2025-04-09 10:41:51 -0700

    Add internal representations of results (e.g. types, effects, constraints, etc.)
    Bug: n/a
    Test: new
    Change-Id: Ibbfc4cc9dc0bd2101a4e01924316b16cb9aa7ffe

---
+++ lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/fx/result/prettyPrinting.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.lint.checks.fx.result
+
+import org.jetbrains.uast.UElement
+
+// everything in this file is just for internal debugging
+
+internal fun Int.subscript(): String =
+  when {
+    this < 0 -> "₋${(-this).subscript()}"
+    else -> {
+      val r = "₀₁₂₃₄₅₆₇₈₉"[this % 10].toString()
+      val q = this / 10
+      if (q > 0) "${q.subscript()}$r" else r
+    }
+  }
+
+private fun String.abbrev(maxLen: Int = 30): String =
+  when {
+    length <= maxLen -> this
+    else -> "${substring(0, maxLen)}…"
+  }
+
+internal fun UElement.renderAbbrev(maxLen: Int = 30): String =
+  "⟪${asSourceString().abbrev(maxLen)}⟫"
+
+internal fun String.bold() = map { it.bold() }.joinToString("")
+
+private fun Char.bold(): String {
+  val boldLowers = "𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳"
+  val boldUppers = "𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙"
+  val boldDigits = "𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗"
+  fun String.unicodeAt(i: Int) = substring(2 * i, 2 * i + 2)
+  return when (this) {
+    in 'a'..'z' -> boldLowers.unicodeAt(this - 'a')
+    in 'A'..'Z' -> boldUppers.unicodeAt(this - 'A')
+    in '0'..'9' -> boldDigits.unicodeAt(this - '0')
+    else -> "$this"
+  }
+}
---
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/fx/result/TypeEffectConstraintLatticeTest.kt
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.lint.checks.fx.result
+
+import com.android.tools.lint.checks.fx.result.Type.Sym
+import com.android.tools.lint.checks.fx.result.Type.Sym.Invoke
+import com.android.tools.lint.checks.fx.utils.LatticeTest
+import com.android.tools.lint.checks.fx.utils.UnboundedSet
+import com.android.tools.lint.checks.fx.utils.possibilityLattice
+import com.google.common.truth.Truth
+import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.collections.immutable.persistentSetOf
+import org.junit.Test
+
+private typealias IntsFx = UnboundedSet<Int>
+
+private val UnitTypeEffectConstraintLattice = TypeEffectConstraintLattice(possibilityLattice<Int>())
+
+class TypeLatticeTest :
+  LatticeTest<Type<IntsFx>>(
+    lattice = UnitTypeEffectConstraintLattice.typeLattice,
+    poolInits = listOf(Type.Int, Type.Boolean, Sym.Param("x"), Sym.Param("x")["f", Sym.Param("y")]),
+  ) {
+
+  @Test
+  fun `widening works`() {
+    Truth.assertThat(widen(Type.Unit, Type.Unit)).isEqualTo(Type.Unit)
+    Truth.assertThat(widen(Type.Int, Type.Boolean)).isEqualTo(Type.Int join Type.Boolean)
+    Truth.assertThat(widen(Type.Int, Type.Int join Type.Boolean))
+      .isEqualTo(Type.Int join Type.Boolean)
+
+    Truth.assertThat(widen(x, x)).isEqualTo(x)
+    Truth.assertThat(widen(x, y)).isEqualTo(x join y)
+    Truth.assertThat(widen(x, x join y["f"])).isEqualTo(x join y["f"])
+
+    // x
+    // y.f(y).g(x)
+    // -->
+    // μα. x ⊔ y.f(y).g(α)
+    testInductiveWidening(x, y["f", y]["g", x] to fix { listOf(x, y["f", y]["g", it]) })
+
+    // x
+    // x.f(x.g())
+    // -->
+    // μα. x ⊔ α.f(α.g())
+    testInductiveWidening(x, x["f", x["g"]] to fix { listOf(x, it["f", it["g"]]) })
+
+    // x
+    // x.f()
+    // x.g()
+    // -->
+    // μα. x ⊔ α.f() ⊔ α.g()
+    testInductiveWidening(
+      x,
+      x["f"] to fix { listOf(x, it["f"]) },
+      x["g"] to fix { listOf(x, it["f"], it["g"]) },
+    )
+
+    // x.h()
+    // x.h().f().g()
+    // x.h().f().g().f().g()
+    // -->
+    // μα. x.h() ⊔ α.f().g()
+    testInductiveWidening(
+      x["h"],
+      x["h"]["f"]["g"] to fix { listOf(x["h"], it["f"]["g"]) },
+      x["h"]["f"]["g"]["f"]["g"] to fix { listOf(x["h"], it["f"]["g"]) },
+    )
+  }
+}
+
+class EffectLatticeTest :
+  LatticeTest<Effect<IntsFx>>(
+    lattice = UnitTypeEffectConstraintLattice.effectLattice,
+    poolInits =
+      listOf(
+        Effect(persistentSetOf(1, 2, 3), persistentSetOf(x["f", Type.Int])),
+        Effect(persistentSetOf(2, 3, 4), persistentSetOf(x["f", Type.String])),
+        Effect(persistentSetOf(3, 4, 5), persistentSetOf(x["f", Type.Int, Type.String])),
+      ),
+  ) {
+
+  @Test
+  fun `symbolic invocations summarized`() {
+    val fx1 = Effect(persistentSetOf(1, 2, 3), persistentSetOf(x["f", Type.Int]))
+    val fx2 = Effect(persistentSetOf(2, 3, 4), persistentSetOf(x["f", Type.String]))
+    val fx3 = Effect(persistentSetOf(3, 4, 5), persistentSetOf(x["f", Type.Int, Type.String]))
+    val fx = widen(fx1, widen(fx2, fx3))
+    Truth.assertThat(fx.concrete).isEqualTo(persistentSetOf(1, 2, 3, 4, 5))
+    Truth.assertThat(fx.invocations)
+      .isEqualTo(
+        persistentSetOf(
+          x["f", Type.Union(persistentSetOf(Type.Int, Type.String))],
+          x["f", Type.Int, Type.String],
+        )
+      )
+  }
+}
+
+class ConstraintLatticeTest :
+  LatticeTest<Constraint<IntsFx>>(
+    lattice = UnitTypeEffectConstraintLattice.constraintLattice,
+    poolInits =
+      listOf(
+        Constraint(
+          persistentMapOf(x["f"] to persistentSetOf(1, 2, 3)),
+          persistentMapOf(x["f"] to persistentSetOf(y["g"])),
+        ),
+        Constraint(
+          persistentMapOf(x["g"] to persistentSetOf(2, 3, 4)),
+          persistentMapOf(x["f"] to persistentSetOf(x["g"])),
+        ),
+        Constraint(
+          persistentMapOf(x["f"] to persistentSetOf(2, 3, 4)),
+          persistentMapOf(x["f"] to persistentSetOf(y["h"])),
+        ),
+      ),
+  ) {
+
+  @Test
+  fun `joining over constraints`() {
+    val c1 =
+      Constraint(
+        persistentMapOf(x["f"] to persistentSetOf(1, 2, 3)),
+        persistentMapOf(y["g"] to persistentSetOf(x["h"])),
+      )
+    val c2 =
+      Constraint(
+        persistentMapOf(x["f"] to persistentSetOf(2, 3, 4)),
+        persistentMapOf(y["g"] to persistentSetOf(x["m"])),
+      )
+
+    val c = c1 join c2 // Most permissive constraint implied by `c1` and `c2`
+    Truth.assertThat(c.concreteUpperbounds)
+      .isEqualTo(persistentMapOf(x["f"] to persistentSetOf(2, 3)))
+    Truth.assertThat(c.symbolicUpperbounds)
+      .isEqualTo(persistentMapOf(y["g"] to persistentSetOf(x["h"], x["m"])))
+  }
+}
+
+// Constructor shorthands
+
+internal val x = Sym.Param("x")
+internal val y = Sym.Param("y")
+internal val z = Sym.Param("z")
+
+internal fun <FX> fix(body: (Sym<FX>) -> Collection<Sym<FX>>) = Sym.Fix(body(Sym.Rec))
+
+internal operator fun <FX> Sym<FX>.get(methodName: String, vararg args: Type<FX>): Invoke<FX> {
+  // Method descriptor. Type signature unimportant for this test.
+  val methodId = MethodId(true, methodName, List(args.size) { null })
+  return Invoke(this, methodId, args.asList())
+}
