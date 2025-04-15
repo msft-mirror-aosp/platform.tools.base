f35ef455101689944f10078c53caf94c44e583e6
Paul Milian <paulmilian@google.com>
2025-04-15 10:45:01 +0100

    Update highest known Wear API version to 36
    Bug: 403612573
    Bug: 403436037
    Test: manually checked that API 36 is available in the NPW
    Change-Id: I2d6ea03d8ffdbdab6acf92631086c930d559307c

--- sdklib/src/main/java/com/android/sdklib/SdkVersionInfo.java
+++ sdklib/src/main/java/com/android/sdklib/SdkVersionInfo.java
@@ -76,7 +76,7 @@ public class SdkVersionInfo {
      * The highest known API level for Wearables. Note the tools at the downloadable system images
      * for wearables to see if there are more recent versions.
      */
-    public static final int HIGHEST_KNOWN_API_WEAR = 35;
+    public static final int HIGHEST_KNOWN_API_WEAR = 36;
 
     /**
      * The lowest active api for wearables. This number will change over time as the distribution of
