434c9d88d34fe16c350df9a2f8ce22c2a4879689
Christophe Rhodes <xof@google.com>
2025-04-28 16:47:17 +0000

    [lint] synchronize access to reservedQuickFixNames and its values
    Bug: 414382194
    Test: none
    Change-Id: I55efc25dcb0999836f5cb84d99567455935fafb1

--- lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/GradleDetector.kt
+++ lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/GradleDetector.kt
@@ -3250,6 +3250,7 @@ open class GradleDetector : Detector(), GradleScanner, TomlScanner, XmlScanner {
      * creating a clash if all fixes are applied.
      */
     var reservedQuickfixNames: MutableMap<String, MutableSet<String>>? = null
+    val reservedQuickfixNamesLock: Any = Object()
 
     /** targetSdkVersion about to expire */
     @JvmField
