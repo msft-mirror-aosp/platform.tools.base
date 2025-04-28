757f4ed16573576dc86d4cae01fcf57b394db24c
David <sholoyed@google.com>
2025-04-28 15:54:53 +0000

    Add multiple deployment property
    Introduces support for multiple device specifications in deployment. Adds handling for IDE_APK_SELECT_CONFIG and IDE_APK_SELECT_MULTIPLE_DEVICE_SPECS properties.
    
    Bug:400374428
    Test: N/A
    Change-Id: Id6a95f9ec5b1f9709074d21b00e92852672a0e22

--- build-system/gradle-core/src/main/java/com/android/build/gradle/options/StringOption.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/options/StringOption.kt
@@ -19,6 +19,7 @@ package com.android.build.gradle.options
 import com.android.builder.model.AndroidProject
 import com.android.builder.model.PROPERTY_APK_LOCATION
 import com.android.builder.model.PROPERTY_APK_SELECT_CONFIG
+import com.android.builder.model.PROPERTY_APK_SELECT_MULTIPLE_DEVICE_SPECS
 import com.android.builder.model.PROPERTY_ATTRIBUTION_FILE_LOCATION
 import com.android.builder.model.PROPERTY_BUILD_ABI
 import com.android.builder.model.PROPERTY_BUILD_API_CODENAME
@@ -57,6 +58,7 @@ enum class StringOption(
 
     // device config for ApkSelect
     IDE_APK_SELECT_CONFIG(PROPERTY_APK_SELECT_CONFIG, ApiStage.Stable),
+    IDE_APK_SELECT_MULTIPLE_DEVICE_SPECS(PROPERTY_APK_SELECT_MULTIPLE_DEVICE_SPECS, ApiStage.Experimental),
 
     // location where to write the APK/BUNDLE
     IDE_APK_LOCATION(PROPERTY_APK_LOCATION, ApiStage.Stable),
