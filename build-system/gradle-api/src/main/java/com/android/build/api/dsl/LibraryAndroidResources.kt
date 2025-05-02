453589c88f6869c0ecdb6b8eb998be34c45a97b6
Hakan Mehmed <hmehmed@google.com>
2025-05-02 11:00:36 +0100

    Stabilize androidResources.enable API for libraries
    Deprecates buildFeatures.androidResources for libraries
    
    Bug: na
    Test: current.txt
    Change-Id: I70710f26a887885ff65da9fd45eabec812b81c6b

--- build-system/gradle-api/src/main/java/com/android/build/api/dsl/LibraryAndroidResources.kt
+++ build-system/gradle-api/src/main/java/com/android/build/api/dsl/LibraryAndroidResources.kt
@@ -16,8 +16,6 @@
 
 package com.android.build.api.dsl
 
-import org.gradle.api.Incubating
-
 /**
  * DSL object for configuring Android resource options for Library plugins.
  * This is accessed via [LibraryExtension.androidResources]
@@ -27,7 +25,5 @@ interface LibraryAndroidResources : AndroidResources {
      * Flag to enable Android resource processing in this library module
      * Default value is 'true' for plain android libraries and 'false' for multiplatform libraries.
      */
-    @set:Incubating
-    @get:Incubating
     var enable: Boolean
 }
