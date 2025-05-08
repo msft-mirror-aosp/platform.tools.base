7da11347e5c90ca56e68629c43a70a4b16461991
Christophe Rhodes <xof@google.com>
2025-05-08 12:13:16 +0000

    Improve the implementation of GoogleMavenArtifactId#find
    Look up entries in a Map computed once, rather than matching on a
    sequence, to make it less painful in case find() gets called in more
    places than currently.  Add tests of expected behaviour.
    
    Bug: none filed
    Test: added
    Change-Id: I3625b238dec1e9befbd4ec10c8d271a5dbb4a335

--- sdk-common/src/main/java/com/android/ide/common/repository/GoogleMavenArtifactId.kt
+++ sdk-common/src/main/java/com/android/ide/common/repository/GoogleMavenArtifactId.kt
@@ -304,11 +304,14 @@ enum class GoogleMavenArtifactId(val mavenGroupId: String, val mavenArtifactId: 
   override fun toString(): String = displayName
 
   companion object {
+    private val ENTRIES_BY_MODULEID = entries.associateBy { it.toString() }
+    private val ENTRIES_BY_GROUP_ARTIFACT_PAIR = entries.associateBy { it.mavenGroupId to it.artifactId }
+
     @JvmStatic fun find(moduleId: String): GoogleMavenArtifactId? =
-        values().asSequence().find { it.toString() == moduleId }
+        ENTRIES_BY_MODULEID[moduleId]
 
     @JvmStatic fun find(groupId: String, artifactId: String): GoogleMavenArtifactId? =
-        values().asSequence().find { it.mavenGroupId == groupId && it.mavenArtifactId == artifactId }
+        ENTRIES_BY_GROUP_ARTIFACT_PAIR[groupId to artifactId]
 
     @JvmStatic fun androidxIdOf(id: GoogleMavenArtifactId): GoogleMavenArtifactId =
         find(AndroidxNameUtils.getCoordinateMapping(id.toString())) ?: id
