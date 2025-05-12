3c062d0987deb90799f06357bc19b9e1a4a5b04d
Christophe Rhodes <xof@google.com>
2025-05-12 11:23:02 +0000

    Provide a static `find` for WellKnownMavenArtifactId
    This isn't desperately well-founded, in that it would be fairly easy
    to forget to add a suitable initializer block (or equivalent) for an
    implementation class.  However, it is mitigated by having a sealed
    interface at the top of the heirarchy, so any mistakes are in fact
    ours.  (This is probably a temporary measure while converting project
    system APIs away from GradleCoordinate, so the lack of soundness is
    acceptable).
    
    Bug: 279886738
    Test: added
    Change-Id: If151a297460ac42c38927710e3b6b40d1d879cc3

---
+++ sdk-common/src/test/java/com/android/ide/common/repository/WellKnownMavenArtifactIdTest.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.ide.common.repository
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class WellKnownMavenArtifactIdTest {
+    @Test
+    fun testFindKotlinArtifacts() {
+        assertThat(WellKnownMavenArtifactId.find("org.jetbrains.kotlin", "kotlin-stdlib"))
+            .isEqualTo(WellKnownMavenArtifactId.KOTLIN_STDLIB)
+        assertThat(WellKnownMavenArtifactId.find("org.jetbrains.kotlin", "kotlin-reflect"))
+            .isEqualTo(WellKnownMavenArtifactId.KOTLIN_REFLECT)
+    }
+
+    @Test
+    fun testDoesNotFindNonexistentKotlinArtifact() {
+        assertThat(WellKnownMavenArtifactId.find("org.jetbrains.kotlin", "kotlin-nonexistent"))
+            .isNull()
+    }
+
+    @Test
+    fun testFindTfliteArtifacts() {
+        assertThat(WellKnownMavenArtifactId.find("org.tensorflow", "tensorflow-lite-gpu"))
+            .isEqualTo(WellKnownMavenArtifactId.TFLITE_GPU)
+        assertThat(WellKnownMavenArtifactId.find("org.tensorflow", "tensorflow-lite-metadata"))
+            .isEqualTo(WellKnownMavenArtifactId.TFLITE_METADATA)
+        assertThat(WellKnownMavenArtifactId.find("org.tensorflow", "tensorflow-lite-support"))
+            .isEqualTo(WellKnownMavenArtifactId.TFLITE_SUPPORT)
+    }
+
+    @Test
+    fun testDoesNotFindNonexistentTfliteArtifact() {
+        assertThat(WellKnownMavenArtifactId.find("org.tensorflow", "tensorflow-lite-nonexistent"))
+            .isNull()
+    }
+
+    @Test
+    fun testFindGMavenArtifacts() {
+        assertThat(WellKnownMavenArtifactId.find("com.android.support", "animated-vector-drawable"))
+            .isEqualTo(GoogleMavenArtifactId.SUPPORT_ANIMATED_VECTOR_DRAWABLE)
+        assertThat(WellKnownMavenArtifactId.find("androidx.core", "core-ktx"))
+            .isEqualTo(GoogleMavenArtifactId.ANDROIDX_CORE_KTX)
+    }
+}
