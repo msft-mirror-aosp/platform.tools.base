9ab1d2bc79f1a57a7dc973d678362d30f0e38b11
Jerome Dochez <jedo@google.com>
2025-05-14 13:51:48 -0700

    Revert "Initial introduction of the `suites` DSL"
    This reverts commit c0a499ff40dc85b364957898e8e03f6629df9dbf.
    
    Test: Existing
    Bug: N/A
    
    Change-Id: Ie7504f00d03b9eff48f2a8631bb097cff126e38e

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/TestOptions.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/TestOptions.kt
@@ -16,10 +16,8 @@
 
 package com.android.build.gradle.internal.dsl
 
-import com.android.build.api.dsl.AgpTestSuite
 import com.android.build.api.dsl.Device
 import com.android.build.api.dsl.DeviceGroup
-import com.android.build.gradle.internal.dsl.decorator.annotation.WithLazyInitialization
 import com.android.build.gradle.internal.services.DslServices
 import com.android.builder.core.DefaultApiVersion
 import com.android.builder.core.apiVersionFromString
@@ -39,13 +37,8 @@ import javax.inject.Inject
 
 abstract class TestOptions @Inject constructor(
     private val dslServices: DslServices
-) : com.android.build.api.dsl.TestOptions {
-
-    @WithLazyInitialization
-    protected fun lazyInit() {
-        suites.registerBinding(AgpTestSuite::class.java, AgpTestSuiteImpl::class.java)
-    }
-
+) :
+    com.android.build.api.dsl.TestOptions {
     private val executionConverter = HelpfulEnumConverter<Execution>(Execution::class.java)
 
     private var _execution = Execution.HOST
