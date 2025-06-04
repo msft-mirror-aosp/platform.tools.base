7a985c4a654c4ef0a1523f29c19d935e925e56f3
Tom Renn <tomrenn@google.com>
2025-06-04 15:09:20 -0700

    Revert^2 "Use --modified-files to speed up bazel-diff"
    This reverts commit 4c24be8e8d1440074a54cccccc690679589ebccf.
    
    Reason for revert: Fixes the list of modified files. The original change
    used a leading '/' which is incorrect, and it did not use OS-specific
    path separators (backslashes for Windows). These are both fixed in the
    write_modified_files method.
    
    Bug: 422485079
    
    Change-Id: I71a6330eaca831052fad488445749c01dbaeea82

--- bazel/ci/presubmit/impacted_targets_test.py
+++ bazel/ci/presubmit/impacted_targets_test.py
@@ -29,6 +29,7 @@ class ImpactedTargetsTest(absltest.TestCase):
         external_repos: Iterable[str],
         path: str,
         deps_output_path: pathlib.Path | None = None,
+        modified_files_path: pathlib.Path | None = None,
     ) -> None:
       del build_env, external_repos
       pathlib.Path(path).write_text(contents)
@@ -165,6 +166,7 @@ class ImpactedTargetsTest(absltest.TestCase):
         impacted_targets._LOCAL_REPOSITORIES,
         mock.ANY,
         deps_output_path=None,
+        modified_files_path=mock.ANY,
     )
 
 
