f66b0909d7491dabf59f97a430a7d241746f05de
Tom Renn <tomrenn@google.com>
2025-06-05 13:00:52 -0700

    Add binaries as runtime data
    This avoids needing to involve bazel to get the bazel-diff and
    logs-collector commands, saving overhead cost. It moves building the
    binaries in the same invocation as the ci script.
    
    Change-Id: Iee6dcec661e43a79964ea5a7f8388badf3c37673

--- bazel/ci/presubmit/bazel_diff.py
+++ bazel/ci/presubmit/bazel_diff.py
@@ -1,11 +1,22 @@
 """Module for running bazel-diff."""
 
 import logging
+import os
 import pathlib
+import subprocess
 import time
 from typing import Sequence
 
 from tools.base.bazel.ci import bazel
+
+
+def _run(cmd: Sequence[str], timeout: int = 300):
+  logging.info('Running command: %s', cmd)
+  return subprocess.run(
+      cmd,
+      check=True,
+      timeout=timeout,
+  )
 
 
 def generate_hash_file(
@@ -18,8 +29,7 @@ def generate_hash_file(
   """Generates the hash file for the current build."""
   start = time.time()
   args = [
-      '//tools/base/bazel:bazel-diff',
-      '--',
+      os.environ['BAZEL_DIFF_BINARY'],
       'generate-hashes',
       '--bazelPath',
       build_env.bazel_path,
@@ -33,10 +43,7 @@ def generate_hash_file(
   if modified_files_path:
     args.extend(['--modified-filepaths', str(modified_files_path)])
   args.append(str(output_path))
-  build_env.bazel_run(
-      *args,
-      timeout=300,
-  )
+  _run(args)
   end = time.time()
   logging.info('generate-hashes took %d seconds', end - start)
 
@@ -50,9 +57,8 @@ def get_impacted_targets(
 ) -> None:
   """Generates the list of impacted targets given base and current hash file paths."""
   start = time.time()
-  build_env.bazel_run(
-      '//tools/base/bazel:bazel-diff',
-      '--',
+  _run([
+      os.environ['BAZEL_DIFF_BINARY'],
       '--verbose',
       'get-impacted-targets',
       '--startingHashes',
@@ -63,7 +69,6 @@ def get_impacted_targets(
       str(dep_edges_path),
       '--output',
       str(output_path),
-      timeout=300,
-  )
+  ])
   end = time.time()
   logging.info('get-impacted-targets took %d seconds', end - start)
