e538f29c5354870c54431efdcbc3e9470187e5cc
Hakan Mehmed <hmehmed@google.com>
2025-06-16 15:31:24 +0000

    Fix kmp connected test
    Bug: 425288255
    Test: this
    Change-Id: Ibdd35b49e021bc75eb7d88c65bef7301267219cd

--- build-system/integration-test/connected/src/test/java/com/android/build/gradle/integration/connected/library/KotlinMultiplatformAndroidConnectedTest.kt
+++ build-system/integration-test/connected/src/test/java/com/android/build/gradle/integration/connected/library/KotlinMultiplatformAndroidConnectedTest.kt
@@ -25,11 +25,9 @@ import com.android.utils.FileUtils
 import com.google.common.truth.Truth
 import org.junit.Before
 import org.junit.ClassRule
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
-@Ignore("b/425288255")
 class KotlinMultiplatformAndroidConnectedTest {
 
     companion object {
