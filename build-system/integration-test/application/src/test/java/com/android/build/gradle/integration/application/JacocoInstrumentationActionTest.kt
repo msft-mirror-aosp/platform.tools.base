ca7c7704d1b472624cff8e5b2e61bfd0f1a84a34
Rahul Dubey <rkdubey@google.com>
2025-06-18 16:53:58 +0000

    Fix the regex pattern bug for jacoco code coverage
    Test: Unit test included. Also reproduced the error in a test app and
    resolved it with this change.
    
    Bug: 380291901
    
    Change-Id: I152bad7efd708d62bb2a8aeb43581db405b948be

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/JacocoInstrumentationActionTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/JacocoInstrumentationActionTest.kt
@@ -26,9 +26,12 @@ class JacocoInstrumentationActionTest {
     fun testExpectedInstrumentationAction() {
         val expectedFileAndAction = mapOf(
             "com/example/R.class" to JacocoTask.Action.COPY,
+            "com/example/RandomClassName.class" to JacocoTask.Action.INSTRUMENT,
             "com/example/R\$color.class" to JacocoTask.Action.COPY,
+            "com/example/R\$color" to JacocoTask.Action.IGNORE,
             "com/example/Manifest.class" to JacocoTask.Action.COPY,
             "com/example/Manifest\$details.class" to JacocoTask.Action.COPY,
+            "com/example/ManifestDetails.class" to JacocoTask.Action.INSTRUMENT,
             "com/example/BuildConfig.class" to JacocoTask.Action.COPY,
             "com/example/APiratesFavouriteLetterIsR.class" to JacocoTask.Action.INSTRUMENT,
             "SomeClass.class" to JacocoTask.Action.INSTRUMENT,
