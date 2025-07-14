2d59206c7bc8ce0d0352a476637a91897d52b035
Christophe Rhodes <xof@google.com>
2025-07-14 10:08:27 +0000

    Bump GRADLE_PLUGIN_MINIMUM_VERSION
    Also bump GRADLE_PLUGIN_MINIMUM_FORCED_UPGRADE_VERSION constant,
    effectively moving AGP versions below 4.0.0 into "Controlled Failure"
    state.
    
    Bug: none filed
    Test: tests adjusted
    Change-Id: I1e94025967ed64faf530f255bbe1d8f1e1079efe

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2928,13 +2928,13 @@ public final class SdkConstants {
      * display a diagnostic informing the user that this project's version AGP is unsupported, but
      * at some point that might fail.
      */
-    public static final String GRADLE_PLUGIN_MINIMUM_FORCED_UPGRADE_VERSION = "3.2.0";
+    public static final String GRADLE_PLUGIN_MINIMUM_FORCED_UPGRADE_VERSION = "4.0.0";
 
     /**
      * The minimum released version of the Android Gradle Plugin that this version of Studio will
      * support. (Support of the development series leading up to it is unknown.)
      */
-    public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "4.0.0";
+    public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "4.1.0";
 
     /**
      * The minimum released version of the Android Gradle Plugin that the next version of Studio
