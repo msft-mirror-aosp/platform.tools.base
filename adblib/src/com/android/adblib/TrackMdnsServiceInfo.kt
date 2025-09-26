5b0f7d7e448b1bdf0105bd16613dca2eabfd7d9d
Sherif Eid <sherifeid@google.com>
2025-09-26 15:21:04 +0100

    Add correct links to protos and documentation related to adb wifi
    The current links aren't up-to-date with the latest mdns changes.
    
    Test: N/A
    Bug: 412571872
    Change-Id: I09f14e04a39b206853a2a5f28d64ab665f713f1a

--- adblib/src/com/android/adblib/TrackMdnsServiceInfo.kt
+++ adblib/src/com/android/adblib/TrackMdnsServiceInfo.kt
@@ -17,9 +17,9 @@ package com.android.adblib
 
 /**
  * mDNS services as returned by ADB "host:track-mdns-services" query. See
- * https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/docs/dev/services.md
+ * https://source.corp.google.com/h/googleplex-android/platform/superproject/main/+/c51903756bff7744c589ca2190df9d161494f37d:packages/modules/adb/docs/dev/services.md
  * and
- * https://cs.android.com/android/platform/superproject/main/+/main:packages/modules/adb/proto/adb_host.proto
+ * https://source.corp.google.com/h/googleplex-android/platform/superproject/main/+/124967b0fb712eb69ca625a11448e3f169e07e2c:packages/modules/adb/proto/adb_host.proto
  * for more information.
  */
 data class MdnsServices(
