249e94184438f73ecfbf1610caefdc2f9d579fe5
bingran <bingran@google.com>
2025-09-30 11:43:59 -0700

    Fix initExtensionFromSettings for KMP
    Bug: 446492061
    Test: SettingsSdkTest
    Change-Id: I2dea915f52ca4287aef97fe08ebc10b9421d24fb

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/plugins/KotlinMultiplatformAndroidPlugin.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/plugins/KotlinMultiplatformAndroidPlugin.kt
@@ -64,6 +64,8 @@ import com.android.build.gradle.internal.dependency.SingleVariantProductFlavorRu
 import com.android.build.gradle.internal.dependency.VariantDependencies
 import com.android.build.gradle.internal.dsl.DependencySelectionImpl
 import com.android.build.gradle.internal.dsl.KotlinMultiplatformAndroidLibraryExtensionImpl
+import com.android.build.gradle.internal.dsl.CompileSdkVersionImpl
+import com.android.build.gradle.internal.dsl.MinSdkVersionImpl
 import com.android.build.gradle.internal.dsl.ModulePropertyKey
 import com.android.build.gradle.internal.dsl.SdkComponentsImpl
 import com.android.build.gradle.internal.getManagedDeviceAvdFolder
@@ -302,24 +304,32 @@ class KotlinMultiplatformAndroidPlugin @Inject constructor(
     private fun KotlinMultiplatformAndroidLibraryExtension.initExtensionFromSettings(
         settings: SettingsExtension
     ) {
-        settings.compileSdk?.let { compileSdk ->
-            this.compileSdk = compileSdk
-
-            settings.compileSdkExtension?.let { compileSdkExtension ->
-                this.compileSdkExtension = compileSdkExtension
+        settings.compileSdk compileSdkSettings@{
+            this@initExtensionFromSettings.compileSdk {
+                val settingsCompileSdkVersion = this@compileSdkSettings.version
+                settingsCompileSdkVersion?.let {
+                    this.version = CompileSdkVersionImpl(
+                        apiLevel = it.apiLevel,
+                        minorApiLevel = it.minorApiLevel,
+                        sdkExtension = it.sdkExtension,
+                        codeName = it.codeName,
+                        addonName = it.addonName,
+                        vendorName = it.vendorName
+                    )
+                }
             }
         }
 
-        settings.compileSdkPreview?.let { compileSdkPreview ->
-            this.compileSdkPreview = compileSdkPreview
-        }
-
-        settings.minSdk?.let { minSdk ->
-            this.minSdk = minSdk
-        }
-
-        settings.minSdkPreview?.let { minSdkPreview ->
-            this.minSdkPreview = minSdkPreview
+        settings.minSdk minSdkSettings@ {
+            this@initExtensionFromSettings.minSdk {
+                val settingsMinSdkVersion = this@minSdkSettings.version
+                settingsMinSdkVersion?.let {
+                    this.version = MinSdkVersionImpl(
+                        apiLevel = it.apiLevel,
+                        codeName = it.codeName
+                    )
+                }
+            }
         }
 
         settings.buildToolsVersion.let { buildToolsVersion ->
