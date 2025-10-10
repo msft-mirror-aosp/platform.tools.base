c2a59303e448c4c28adfce0b9ec5cb32ff513677
Scott Pollom <spollom@google.com>
2025-10-10 15:42:51 -0700

    Add dependencies and tweak some versions for arch sample template
    Add many dependencies that the agent was adding for tier-2 and tier-4
    apps.
    
    Tweak some version numbers.
    
    Add the internet permission to the manifest.
    
    Note: the versions used here are not always the latest versions because
    the version of Kotlin being used is 2.0.21, and some of the latest
    versions are not compatible with Kotlin 2.0.21.
    
    Bug: 449546776
    Test: manual
    Change-Id: I2d7261e4689bc9e21fa43f724104e1c67813cdd5

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/archStarterActivity/archStarterActivityRecipe.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/archStarterActivity/archStarterActivityRecipe.kt
@@ -46,17 +46,22 @@ fun RecipeExecutor.archStarterActivityRecipe(
   val (_, srcOut, resOut, _) = moduleData
   addAllKotlinDependencies(moduleData)
 
-  addDependency(mavenCoordinate = "androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-  addDependency(mavenCoordinate = "androidx.activity:activity-compose:1.10.1")
+  val lifecycleVersion = "2.8.7"
+  addDependency("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+  addDependency("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+  addDependency("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+
+  val activityVersion = "1.10.1"
+  addDependency("androidx.activity:activity-compose:$activityVersion")
 
   // Add Compose dependencies, using the BOM to set versions
   addComposeDependencies(moduleData)
 
-  val hiltVersion = "2.56.1"
+  val hiltVersion = "2.57.2"
   addPlugin(
     "com.google.dagger.hilt.android",
     "com.google.dagger:hilt-android-gradle-plugin",
-    "$hiltVersion",
+    hiltVersion,
   )
 
   // KSP is needed for Hilt and Room
@@ -68,13 +73,17 @@ fun RecipeExecutor.archStarterActivityRecipe(
     "${moduleData.projectTemplateData.kotlinVersion}-+",
   )
 
-  addDependency("androidx.hilt:hilt-navigation-compose:1.2.0")
-  addDependency("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-  addDependency("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
-  addDependency("androidx.navigation:navigation-compose:2.8.9")
-  addDependency("androidx.room:room-runtime:2.7.0")
-  addDependency("androidx.room:room-ktx:2.7.0")
-  addDependency("androidx.room:room-compiler:2.7.0", configuration = "ksp")
+  val hiltNavigationComposeVersion = "1.2.0"
+  addDependency("androidx.hilt:hilt-navigation-compose:$hiltNavigationComposeVersion")
+
+  val navigationVersion = "2.8.9"
+  addDependency("androidx.navigation:navigation-compose:$navigationVersion")
+
+  val roomVersion = "2.7.0"
+  addDependency("androidx.room:room-runtime:$roomVersion")
+  addDependency("androidx.room:room-ktx:$roomVersion")
+  addDependency("androidx.room:room-compiler:$roomVersion", configuration = "ksp")
+
   addDependency("com.google.dagger:hilt-android:$hiltVersion")
   addDependency("com.google.dagger:hilt-android-compiler:$hiltVersion", configuration = "ksp")
   addDependency("com.google.dagger:hilt-compiler:$hiltVersion", configuration = "ksp")
@@ -88,14 +97,55 @@ fun RecipeExecutor.archStarterActivityRecipe(
     configuration = "testImplementation",
   )
   addDependency("androidx.test:core:1.6.1", configuration = "testImplementation")
-  addDependency("androidx.test.ext:junit:1.2.1", configuration = "testImplementation")
+  addDependency("androidx.test.ext:junit:1.3.0", configuration = "testImplementation")
   addDependency("androidx.test:runner:1.6.2", configuration = "androidTestImplementation")
 
   addDependency(mavenCoordinate = "androidx.compose.material3:material3")
   addDependency(mavenCoordinate = "androidx.compose.material:material-icons-core")
   addDependency(mavenCoordinate = "androidx.compose.material:material-icons-extended")
 
-  addDependency("io.coil-kt:coil-compose:2.7.0")
+  // Coil
+  val coilVersion = "2.7.0"
+  addDependency("io.coil-kt:coil-compose:$coilVersion")
+
+  // Retrofit
+  val retroFitVersion = "2.12.0"
+  addDependency("com.squareup.retrofit2:retrofit:$retroFitVersion")
+  addDependency("com.squareup.retrofit2:converter-moshi:$retroFitVersion")
+
+  // Coroutines
+  val coroutinesVersion = "1.10.2"
+  addDependency("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+  addDependency("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+  // Accompanist
+  val accompanistVersion = "0.37.3"
+  addDependency("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+
+  // Play Services
+  val playServicesLocationVersion = "21.3.0"
+  addDependency("com.google.android.gms:play-services-location:$playServicesLocationVersion")
+
+  // CameraX
+  val cameraVersion = "1.5.0"
+  addDependency("androidx.camera:camera-camera2:$cameraVersion")
+  addDependency("androidx.camera:camera-lifecycle:$cameraVersion")
+  addDependency("androidx.camera:camera-view:$cameraVersion")
+  addDependency("androidx.camera:camera-core:$cameraVersion")
+
+  // OkHttp
+  val okHttpVersion = "4.10.0"
+  addDependency("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
+  addDependency("com.squareup.okhttp3:okhttp:$okHttpVersion")
+
+  // Moshi
+  val moshiVersion = "1.15.2"
+  addDependency("com.squareup.moshi:moshi-kotlin:$moshiVersion")
+  addDependency("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion", configuration = "ksp")
+
+  // DataStore
+  val dataStoreVersion = "1.1.7"
+  addDependency("androidx.datastore:datastore-preferences:$dataStoreVersion")
 
   copy(File("arch-sample-activity").resolve("drawable"), resOut.resolve("drawable"))
 
@@ -126,6 +176,7 @@ fun RecipeExecutor.archStarterActivityRecipe(
     mergeXml(
       """
           <manifest xmlns:android ="http://schemas.android.com/apk/res/android">
+            <uses-permission android:name="android.permission.INTERNET" />
             <application android:name=".$appName"/>
           </manifest>
       """
