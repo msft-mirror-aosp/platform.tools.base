04afb1c02694b2532b9b6c4fbdbdef00a169d7d2
Tom Renn <tomrenn@google.com>
2025-10-15 11:02:50 -0700

    Write ci.bazelrc with --credential_helper
    Change-Id: I3ea6cfc1074f95e0c5b47a0c42efcfc1c6f64b28

--- bazel/ci/ci.cmd
+++ bazel/ci/ci.cmd
@@ -1,4 +1,9 @@
-set WORKSPACE=%~dp0\..\..\..\..
+set WORKSPACE_DIR=%~dp0\..\..\..\..
 set PATH=%PATH%;%cd%\prebuilts\studio\jdk\jbr-next\win\bin
-set BAZELISK=%WORKSPACE%\prebuilts\tools\windows-x86_64\bazel\bazelisk.exe
+set BAZELISK=%WORKSPACE_DIR%\prebuilts\tools\windows-x86_64\bazel\bazelisk.exe
+
+if defined BUILD_NUMBER (
+  echo common --credential_helper=*.pkg.dev=%%workspace%%/build/bazel/tools/ci_credhelper.cmd > ci.bazelrc
+)
+
 %BAZELISK% --max_idle_secs=60 run --config=ci --config=remote-exec //tools/base/bazel/ci -- %1
