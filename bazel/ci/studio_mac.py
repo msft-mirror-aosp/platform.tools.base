61df00703a273f19968a19f84c5b73e943af06d6
Tom Renn <tomrenn@google.com>
2025-10-16 14:54:10 -0700

    Reduce the targets run by studio-mac
    Stop using //tools/..., and require either specific build targets, or tests to be tagged with ci:studio-mac.
    
    Change-Id: I37b0372825f5cdd9c9768d6cb176b8a13cd5e508

--- bazel/ci/studio_mac.py
+++ bazel/ci/studio_mac.py
@@ -16,22 +16,22 @@ _ARTIFACTS = [
 
 def studio_mac(build_env: bazel.BuildEnv) -> None:
   """Runs studio-mac target."""
+  # List attributes, like tags, are converted into into strings in the format:
+  # [value1, value2]. So a tag ends with a comma (,) or closing bracket (]).
+  query = [r'attr(tags, "ci:studio-mac[,\]]", //tools/...)']
+  test_targets = (
+      build_env.bazel_query(*query).stdout.decode('utf-8').splitlines()
+  )
+  targets = test_targets + [
+      '//tools/vendor/google/skia:skiaparser',
+      '//tools/vendor/google/skia:skia_test_support',
+      '//tools/base/profiler/native/trace_processor_daemon',
+      '//tools/base/profiler/native/sherlock:sherlock_trace_processor',
+  ]
   flags = build_flags(
       build_env,
       'ci:studio-mac',
   )
-  targets = [
-      '//tools/...',
-      '//tools/adt/idea/studio:android-studio.linux.zip',
-      '//tools/adt/idea/studio:android-studio.mac.zip',
-      '//tools/adt/idea/studio:android-studio.mac_arm.zip',
-      '//tools/adt/idea/studio:android-studio.win.zip',
-      '-//tools/vendor/google/aswb/...',
-      '-//tools/vendor/google3/aswb/...',
-      '-//tools/adt/idea/aswb/...',
-      '//tools/base/profiler/native/trace_processor_daemon',
-      '//tools/base/profiler/native/sherlock:sherlock_trace_processor',
-  ]
   result = studio.run_bazel_test(build_env, flags, targets)
   if studio.is_build_successful(result):
     if build_env.dist_dir:
