1bb1c60428ae4147b3844c450abc395b29daf764
Alex Golubev <alexgolubev@google.com>
2025-10-28 14:52:49 -0700

    Make default abiFilter visible to Studio
    Bug: 366238650
    Test: existing
    Change-Id: I59d11eab621a94d57ecf5db4168bd337d7401cf6

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/ide/v2/ModelBuilder.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/ide/v2/ModelBuilder.kt
@@ -1240,7 +1240,7 @@ class ModelBuilder<ExtensionT : CommonExtension>(
 
             applicationId = getApplicationId(component),
 
-            abiFilters = (component as? ConsumableCreationConfig)?.nativeBuildCreationConfig?.userDefinedAbis ?: emptySet(),
+            abiFilters = (component as? ConsumableCreationConfig)?.nativeBuildCreationConfig?.supportedAbis ?: emptySet(),
             testInfo = testInfo,
             bundleInfo = getBundleInfo(component),
             codeShrinker = CodeShrinker.R8.takeIf {
