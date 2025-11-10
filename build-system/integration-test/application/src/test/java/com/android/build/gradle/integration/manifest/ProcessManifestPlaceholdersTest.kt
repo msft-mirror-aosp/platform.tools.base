621ba70b49ed32334d072ebda8530682a7d1f885
Jerome Dochez <jedo@google.com>
2025-11-10 14:50:36 -0800

    added a test to ensure placeholders in variant API are picked up.
    A bug was filed that it is not working but I cannot reproduce the
    problem and added a test to ensure it is working and not regressing.
    
    Test: This is a test
    Bug: 440279747
    Change-Id: I566801acc7371bf917abf0215bae50b2fb14046a

---
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/manifest/ProcessManifestPlaceholdersTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.manifest
+
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+import com.android.build.gradle.integration.common.fixture.project.GradleRule
+import com.android.build.gradle.integration.common.fixture.project.plugins.ApplicationComponentCallback
+import org.gradle.api.Project
+import org.junit.Rule
+import org.junit.Test
+
+class ProcessManifestPlaceholdersTest {
+
+    @get:Rule
+    val rule = GradleRule.from {
+        androidApplication {
+            android {
+               pluginCallbacks += ManifestPlaceHolderProviderCallback::class.java
+            }
+        }.dependencies {
+            api(project(":lib"))
+        }
+        androidLibrary {
+            android {
+                namespace="com.example.text"
+            }
+        }.files {
+            add("src/main/AndroidManifest.xml",
+                """
+                    <manifest xmlns:android="http://schemas.android.com/apk/res/android">
+                        <meta-data android:name="example_meta" android:value="${'$'}{exampleDataPlaceholder}" />
+                        <application />
+                    </manifest>
+                """.trimIndent())
+        }
+    }
+
+    @Test
+    fun testBuild() {
+        rule.build.executor.run("assembleDebug")
+    }
+}
+
+class ManifestPlaceHolderProviderCallback: ApplicationComponentCallback {
+
+    override fun handleExtension(
+        project: Project,
+        androidComponents: ApplicationAndroidComponentsExtension
+    ) {
+        androidComponents.onVariants {
+            it.manifestPlaceholders.put("exampleDataPlaceholder", "exampleDataValue")
+        }
+    }
+
+}
