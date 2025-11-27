c7483d905757731bb750004b8f0744a0bbcc3d7c
Christophe Rhodes <xof@google.com>
2025-11-27 11:10:06 +0000

    Remove useless TemporaryProjectModification in MessageRewriteTest
    Change I4937eae497a841786cd7d2c0e10bc597020fd644, in converting from
    Groovy, also changed a ClassRule to a regular Rule, meaning that each
    test runs with its own instance of the project rule.  This further
    means that changes to the project are not visible to any other test.
    
    Bug: none filed
    Test: existing
    Change-Id: I1341f68ad8357c08c5dbd58ec0c27444767c31ea

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/MessageRewriteTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/MessageRewriteTest.kt
@@ -17,9 +17,9 @@
 package com.android.build.gradle.integration.application
 
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
-import com.android.build.gradle.integration.common.fixture.TemporaryProjectModification
 import com.android.build.gradle.integration.common.truth.ScannerSubject
 import com.android.build.gradle.options.BooleanOption
+import com.android.testutils.TestUtils
 import com.android.utils.FileUtils
 import org.junit.Rule
 import org.junit.Test
@@ -33,72 +33,58 @@ class MessageRewriteTest {
 
     @Test
     fun invalidAppLayoutFile() {
-        project.executor()
-            .run("assembleDebug")
-        TemporaryProjectModification.doTest(project) {
-            it.replaceInFile("app/src/main/res/layout/main.xml", "</LinearLayout>", "")
-            val result = project.executor()
-                .with(BooleanOption.IDE_INVOKED_FROM_IDE, true)
-                .expectFailure()
-                .run("assembleF1Debug")
-            checkPathInOutput(
-                    FileUtils.join("app", "src", "main", "res", "layout", "main.xml"),
-                result.stdout)
+        project.executor().run("assembleDebug")
+        project.getSubproject(":app").mainResDir.resolve("layout/main.xml").let {
+            it.writeText(it.readText().replace("</LinearLayout>", ""))
+            TestUtils.waitForFileSystemTick()
         }
+        project.executor()
+            .with(BooleanOption.IDE_INVOKED_FROM_IDE, true)
+            .expectFailure()
+            .run("assembleF1Debug")
+            .let { result ->
+                val path = FileUtils.join("app", "src", "main", "res", "layout", "main.xml")
+                checkPathInOutput(path, result.stdout)
+            }
     }
 
     @Test
     fun nonExistentResourceReferenceInAppLayout() {
-        TemporaryProjectModification.doTest(project) {
-            it.replaceInFile("app/src/main/res/layout/main.xml","@string/app_string", "@string/agloe")
-            val result = project.executor()
-                .expectFailure()
-                .run("assembleDebug")
-            checkPathInOutput(
-                FileUtils.join("app", "src", "main", "res", "layout", "main.xml"),
-                result.stdout)
+        project.getSubproject(":app").mainResDir.resolve("layout/main.xml").let {
+            it.writeText(it.readText().replace("@string/app_string", "@string/agloe"))
+            TestUtils.waitForFileSystemTick()
+        }
+        project.executor().expectFailure().run("assembleDebug").let { result ->
+            val path = FileUtils.join("app", "src", "main", "res", "layout", "main.xml")
+            checkPathInOutput(path, result.stdout)
         }
     }
 
     @Test
     fun nonExistentResourceReferenceInAppValues() {
-        TemporaryProjectModification.doTest(project) {
-            it.replaceInFile("app/src/main/res/values/strings.xml", "string", "")
-            val result = project.executor()
-                .expectFailure()
-                .run("assembleDebug")
-            checkPathInOutput(
-                    FileUtils.join("app", "src", "main", "res", "values", "strings.xml"), result.stderr)
+        project.getSubproject(":app").mainResDir.resolve("values/strings.xml").let {
+            it.writeText(it.readText().replace("string", ""))
+            TestUtils.waitForFileSystemTick()
+        }
+        project.executor().expectFailure().run("assembleDebug").let { result ->
+            val path = FileUtils.join("app", "src", "main", "res", "values", "strings.xml")
+            checkPathInOutput(path, result.stderr)
         }
     }
 
     @Test
     fun nonExistentResourceReferenceInLibLayout() {
-        TemporaryProjectModification.doTest(project) {
-            it.replaceInFile(
-                "lib/src/flavor1/res/layout/lib_main.xml",
-                "@string/lib_string",
-                "@string/agloe"
-            )
-            val result = project.executor()
-                .expectFailure()
-                .run("assembleDebug")
+        project.getSubproject(":lib").projectDir.resolve("src/flavor1/res/layout/lib_main.xml").let {
+            it.writeText(it.readText().replace("@string/lib_string", "@string/agloe"))
+            TestUtils.waitForFileSystemTick()
+        }
+        project.executor().expectFailure().run("assembleDebug").let { result ->
             // b/206624424 - Errors in libraries currently (and incorrectly) rewrite as
             // the packaged res for full builds and merged intermediate filepaths for incremental
             // builds.
-            checkPathInOutput(
-                FileUtils.join(
-                    "lib",
-                    "build",
-                    "intermediates",
-                    "packaged_res",
-                    "flavor1Debug",
-                    "packageFlavor1DebugResources",
-                    "layout",
-                    "lib_main.xml"
-                ),
-                result.stderr
-            )
+            val path = FileUtils.join("lib", "build", "intermediates", "packaged_res",
+                "flavor1Debug", "packageFlavor1DebugResources", "layout", "lib_main.xml")
+            checkPathInOutput(path, result.stdout)
         }
     }
 
