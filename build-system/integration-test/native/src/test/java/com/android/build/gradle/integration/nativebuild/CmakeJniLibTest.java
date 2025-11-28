90d912cadf84e5c4b171e4e01fee8769bd3a230c
Christophe Rhodes <xof@google.com>
2025-11-28 15:11:04 +0000

    Somewhat future-proof JniLibTest native AGP integration tests
    Rewrite the versionCode frobbing to use the new variant API, and
    explicitly give android:export in the android manifest.  Also add a
    test in CmakeJniLibTest to parallel the equivalent Ndk* and NdkBuild*
    tests.
    
    Bug: none filed
    Test: added
    Change-Id: I8114bb2575376c8941c60072cde346dd2a30f46c

--- build-system/integration-test/native/src/test/java/com/android/build/gradle/integration/nativebuild/CmakeJniLibTest.java
+++ build-system/integration-test/native/src/test/java/com/android/build/gradle/integration/nativebuild/CmakeJniLibTest.java
@@ -19,6 +19,7 @@ package com.android.build.gradle.integration.nativebuild;
 import static com.android.build.gradle.integration.common.fixture.GradleTestProject.DEFAULT_NDK_SIDE_BY_SIDE_VERSION;
 import static com.android.build.gradle.integration.common.fixture.model.NativeUtilsKt.dump;
 import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat;
+import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThatApk;
 import static com.android.testutils.truth.PathSubject.assertThat;
 
 import com.android.build.gradle.integration.common.fixture.GradleTestProject;
@@ -81,6 +82,25 @@ public class CmakeJniLibTest {
     @After
     public void cleanUp() {
         project = null;
+    }
+
+    @Test
+    public void checkVersionCode() {
+        GradleTestProject app = project.getSubproject("app");
+        assertThatApk(app.getApk("universal", GradleTestProject.ApkType.DEBUG, "gingerbread"))
+                .hasVersionCode(1000123);
+        assertThatApk(app.getApk("armeabi-v7a", GradleTestProject.ApkType.DEBUG, "gingerbread"))
+                .hasVersionCode(1100123);
+        assertThatApk(app.getApk("x86", GradleTestProject.ApkType.DEBUG, "gingerbread"))
+                .hasVersionCode(1300123);
+        assertThatApk(app.getApk("universal", GradleTestProject.ApkType.DEBUG, "icecreamSandwich"))
+                .hasVersionCode(2000123);
+        assertThatApk(
+                app.getApk(
+                        "armeabi-v7a", GradleTestProject.ApkType.DEBUG, "icecreamSandwich"))
+                .hasVersionCode(2100123);
+        assertThatApk(app.getApk("x86", GradleTestProject.ApkType.DEBUG, "icecreamSandwich"))
+                .hasVersionCode(2300123);
     }
 
     @Test
