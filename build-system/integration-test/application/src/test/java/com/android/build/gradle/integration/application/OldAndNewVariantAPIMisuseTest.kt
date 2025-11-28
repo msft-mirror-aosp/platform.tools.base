97aae565bcf3d0881f0e5b3e0193730a3deb6afd
Christophe Rhodes <xof@google.com>
2025-11-28 11:33:23 +0000

    Special cases for tests of the interaction between old and new API
    Those tests by their very nature require running with the USE_NEW_DSL
    flag set to false.  Separate out tests of interaction misuse from the
    others in VariantApiMisuseTest, adding an entry to the allowlist in
    order to preserve the existing tests wholesale (with only the addition
    of the flag).
    
    Likewise, add the flag to OldAndNewVariantAPIInteractionTest.
    
    Bug: none filed
    Test: none added, none removed
    Change-Id: Ibc2441ee4533cc4f5bd391cdd4695c667d61f580

---
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/OldAndNewVariantAPIMisuseTest.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.application
+
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp
+import com.android.build.gradle.integration.common.utils.TestFileUtils
+import com.android.build.gradle.options.BooleanOption
+import org.junit.Rule
+import org.junit.Test
+
+class OldAndNewVariantAPIMisuseTest {
+    @get:Rule
+    val project =
+        GradleTestProject.builder()
+            .fromTestApp(HelloWorldApp.forPlugin("com.android.application"))
+            .addGradleProperty(BooleanOption.USE_NEW_DSL, false)
+            .create()
+
+    @Test
+    fun beforeVariantsActionAddedFromOldVariantApiBlock() {
+        TestFileUtils.appendToFile(
+            project.buildFile,
+            """
+apply from: "../commonHeader.gradle"
+buildscript { apply from: "../commonBuildScript.gradle" }
+
+apply plugin: 'com.android.application'
+
+android {
+    defaultConfig.minSdkVersion 14
+    compileSdkVersion 30
+}
+
+android {
+    applicationVariants.all {
+        androidComponents.beforeVariants(androidComponents.selector().all(), {
+            System.out.println("This should not execute !")
+        })
+    }
+}
+""")
+        project.executor().expectFailure().run("clean", "assembleDebug").assertErrorContains(
+            "It is too late to add actions as the callbacks already executed."
+        )
+    }
+
+    @Test
+    fun onVariantsActionAddedFromOldVariantApiBlock() {
+        TestFileUtils.appendToFile(
+            project.buildFile,
+            """
+apply from: "../commonHeader.gradle"
+buildscript { apply from: "../commonBuildScript.gradle" }
+
+apply plugin: 'com.android.application'
+
+android {
+    defaultConfig.minSdkVersion 14
+    compileSdkVersion 30
+}
+
+android {
+    applicationVariants.all {
+        androidComponents.onVariants(androidComponents.selector().all(), {
+            System.out.println("This should not execute !")
+        })
+    }
+}
+""")
+        project.executor().expectFailure().run("clean", "assembleDebug").assertErrorContains(
+            "It is too late to add actions as the callbacks already executed."
+        )
+    }
+}
