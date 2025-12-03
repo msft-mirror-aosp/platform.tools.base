a518e22c515fbe2a8479eaabb5be7782732c081e
Alex Golubev <alexgolubev@google.com>
2025-12-03 13:23:44 -0500

    Rename TestApplicationTaskManager.java to kt
    Bug: NA
    Test: NA
    Change-Id: Idc2450a5564fa0dcb5b24dab607f22c4b6e40287

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/TestApplicationTaskManager.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/TestApplicationTaskManager.kt
@@ -169,4 +169,5 @@ public class TestApplicationTaskManager
         return taskFactory.register(
                 new ProcessTestManifest.CreationAction((TestCreationConfig) creationConfig));
     }
+
 }
