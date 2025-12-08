f254d96343f7249dec2a4fb83f9b8ef4a75cf782
Hung Nguyen <hungnv@google.com>
2025-12-08 14:59:03 +0000

    Remove redundant KGP version check
    in the `maybeUseInlineScopesNumbers` method.
    
    AGP 9.0 requires KGP 2.0+, so the version check for KGP 2.0+ in that
    method is no longer needed.
    
    Bug: 372264148
    Test: Existing UseInlineScopesNumbersTest
    Change-Id: Ic4401c9155314ddc1469e16644b3e195794c11a3

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/kgpUtils.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/kgpUtils.kt
@@ -226,6 +226,13 @@ fun addComposeArgsToKotlinCompile(
     task.compilerOptions.freeCompilerArgs.add("-Xallow-unstable-dependencies")
 }
 
+/**
+ * Adds Kotlin compiler argument `-Xuse-inline-scopes-numbers` to improve the debugging experience
+ * in Android Studio (b/372264148).
+ *
+ * Note: When the Kotlin compiler enables this option by default (KT-79401), we can remove this
+ * method.
+ */
 fun maybeUseInlineScopesNumbers(
     task: KotlinCompile,
     creationConfig: ComponentCreationConfig,
@@ -234,11 +241,6 @@ fun maybeUseInlineScopesNumbers(
     // Only use -Xuse-inline-scopes-numbers for APKs. If it's used for an AAR (or any kind of
     // dependency), consumers wouldn't be able to use Kotlin < 2.0.
     if (!creationConfig.componentType.isApk || !creationConfig.debuggable) {
-        return
-    }
-
-    val kotlinVersion = getProjectKotlinPluginKotlinVersion(task.project)
-    if (kotlinVersion == null || !kotlinVersion.isVersionAtLeast(2, 0)) {
         return
     }
 
