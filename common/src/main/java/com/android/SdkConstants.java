abc6c16eac6ebe89c4cf1dc628270391ecf709e0
Christophe Rhodes <xof@google.com>
2025-12-10 13:11:56 +0000

    Drop sync support for AGP versions earlier than 4.2.0
    This should allow deleting Ndk V1 support?
    
    Bug: none filed
    Test: existing
    Change-Id: I5841066e5bd1788ffc1de7a88cdb9b6c4e3d5283

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2926,7 +2926,7 @@ public final class SdkConstants {
      * display a diagnostic informing the user that this project's version AGP is unsupported, but
      * at some point that might fail.
      */
-    public static final String GRADLE_PLUGIN_MINIMUM_FORCED_UPGRADE_VERSION = "4.0.0";
+    public static final String GRADLE_PLUGIN_MINIMUM_FORCED_UPGRADE_VERSION = "4.2.0";
 
     /**
      * The minimum released version of the Android Gradle Plugin that this version of Studio will
