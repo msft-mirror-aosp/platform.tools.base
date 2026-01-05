474f462a52e188c537b0591d0957aeec417ef243
taqihaider <taqihaider@google.com>
2026-01-05 17:52:07 +0530

    Added test project for report aggregation
    Multi-module test project containing unit test and instrumentation tests.
    
    Bug: 473489104
    Test: na
    Change-Id: I267199771a500252f4c6afda35c4526fdd9769a3

---
+++ build-system/integration-test/test-projects/reportAggregation/app/src/test/java/com/example/app/AppJavaUnitTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import android.app.Activity;
+import android.app.Application;
+
+import org.junit.Test;
+
+public class AppJavaUnitTest {
+    @Test
+    public void referenceProductionCode() {
+        assertEquals("AppJavaClass", new AppJavaClass().getName());
+    }
+
+    @Test
+    public void referenceProductionKotlinCode() {
+        assertEquals("AppKotlinClass", new AppKotlinClass().getName());
+    }
+
+    @Test
+    public void mockFinalMethod() {
+        Activity activity = mock(Activity.class);
+        Application app = mock(Application.class);
+        when(activity.getApplication()).thenReturn(app);
+
+        assertSame(app, activity.getApplication());
+
+        verify(activity).getApplication();
+        verifyNoMoreInteractions(activity);
+    }
+}
