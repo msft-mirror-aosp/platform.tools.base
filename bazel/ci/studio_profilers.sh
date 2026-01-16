ec05573089b0c21b49484541f44c552ffb1b24e7
Emre Kultursay <emrekultursay@google.com>
2026-01-16 12:58:16 -0800

    Add Python to PATH
    Bug: 471827355
    Test: locally ran ./studio_profilers.sh
    Change-Id: If98e0883c1731d2978e512c6d126502165955f30

--- bazel/ci/studio_profilers.sh
+++ bazel/ci/studio_profilers.sh
@@ -8,6 +8,12 @@ BUILD_NUMBER="${BUILD_NUMBER:-SNAPSHOT}"
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly ROOT_DIR="$(realpath "${SCRIPT_DIR}/../../../..")"
 
+# Gradle needs JAVA_HOME.
 export JAVA_HOME="${ROOT_DIR}/prebuilts/studio/jdk/jdk17/linux/"
+
+# Sherlock's Gradle build invokes Python sub-processes.
+export PYTHON3_DIR="${ROOT_DIR}/prebuilts/python/linux-x86/bin/"
+export PATH="${PYTHON3_DIR}:${PATH}"
+
 pushd "${ROOT_DIR}/tools/profiler/sherlock-plugin"
 ./gradlew test -Pverbose.test.logging=true
