af36531e0d538593d5815b5a35ab80fcbf474a82
Alex Golubev <alexgolubev@google.com>
2026-01-16 19:36:53 -0500

    Update model builder current model to have keepRules sourceSet
    Bug: 476099690
    Test: Existing
    Change-Id: I0d17f4aa3b6165779d260e4bd5ffab61715c7c48

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/ide/v2/ModelBuilder.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/ide/v2/ModelBuilder.kt
@@ -284,7 +284,7 @@ class ModelBuilder<ExtensionT : CommonExtension>(
          * method not called by current versions of Studio, the MINIMUM_MODEL_CONSUMER version must
          * be increased to exclude all older versions of Studio that called that method.
          */
-        val modelProducer = VersionImpl(21, 1, humanReadable = "Android Gradle Plugin 9.1")
+        val modelProducer = VersionImpl(22, 0, humanReadable = "Android Gradle Plugin 9.1")
         /**
          * The minimum required model consumer version, to allow AGP to control support for older
          * versions of Android Studio.
