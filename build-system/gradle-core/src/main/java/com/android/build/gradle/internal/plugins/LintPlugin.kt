c2779c878db6dc0d9923d205dee175a4a13c9918
Paige Mcauliffe <paigemca@google.com>
2026-01-20 23:06:02 +0000

    Initial configuration of local and aggregate reporting tasks
    Bug: 477318930
    Test: tests will be added when tasks are registered in a follow up
    Change-Id: I24dbc5450bef4c9198a9e2e2dc7e1018290a5c26

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/plugins/LintPlugin.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/plugins/LintPlugin.kt
@@ -51,6 +51,7 @@ import com.android.build.gradle.internal.profile.NoOpAnalyticsService
 import com.android.build.gradle.internal.projectIsolationActive
 import com.android.build.gradle.internal.publishing.AndroidArtifacts
 import com.android.build.gradle.internal.publishing.getAttributes
+import com.android.build.gradle.internal.scope.InternalArtifactType
 import com.android.build.gradle.internal.scope.InternalMultipleArtifactType
 import com.android.build.gradle.internal.scope.InternalMultipleArtifactType.LINT_REPORT_LINT_MODEL
 import com.android.build.gradle.internal.scope.InternalMultipleArtifactType.LINT_VITAL_REPORT_LINT_MODEL
@@ -279,11 +280,16 @@ abstract class LintPlugin : Plugin<Project> {
                     it,
                     artifacts
                 )
-                AndroidLintTask.SingleVariantCreationAction.registerLintReportArtifacts(
+                AndroidLintTask.VariantCreationAction.registerLintReportArtifacts(
                     it,
                     artifacts,
                     null,
-                    projectServices.projectInfo.buildDirectory.dir("reports")
+                    projectServices.projectInfo.buildDirectory.dir("reports"),
+                    "lint-results",
+                    InternalArtifactType.LINT_TEXT_REPORT,
+                    InternalArtifactType.LINT_HTML_REPORT,
+                    InternalArtifactType.LINT_XML_REPORT,
+                    InternalArtifactType.LINT_SARIF_REPORT
                 )
             }
 
