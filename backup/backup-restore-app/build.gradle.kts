7def1301d324221b7bff12af93a54783804ec20b
Alon Albert <aalbert@google.com>
2026-01-28 08:04:08 -0800

    Upgrade AGP to 9.0.0
    Bug: NA
    Test: NA
    Change-Id: I0d914175817dcce17a217090177adb86b761525c

--- backup/backup-restore-app/build.gradle.kts
+++ backup/backup-restore-app/build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
   alias(libs.plugins.androidx.room) apply false
   alias(libs.plugins.ksp) apply false
   alias(libs.plugins.jetbrains.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
 }
+ 
