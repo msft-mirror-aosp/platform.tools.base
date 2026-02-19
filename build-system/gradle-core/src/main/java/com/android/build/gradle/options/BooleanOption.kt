3b1269cac69f11a8a0da2b3d17fc71968ceba588
Hakan Mehmed <hmehmed@google.com>
2026-02-19 13:00:49 +0000

    Remove DISABLE_KOTLIN_ATTRIBUTE gradle property
    This was initially added as an escape hatch, in case configuring kotlin attributes (which is the default behaviour) lead to unexpected behaviour. Now we have the confidence that it works as expected and in fact built in kotlin always configures the kotlin attributes
    
    Bug: 462340125
    Test: na
    Change-Id: Ic938398f3ea63e292ce0e1ed61cde773cff27e6e

--- build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
@@ -199,7 +199,6 @@ enum class BooleanOption(
   ENABLE_LOCAL_TESTING("android.bundletool.enableLocalTesting", false, FeatureStage.Experimental),
   DISABLE_MINSDKLIBRARY_CHECK("android.unsafe.disable.minSdkLibraryCheck", false, FeatureStage.Experimental),
   ENABLE_INSTRUMENTATION_TEST_DESUGARING("android.experimental.library.desugarAndroidTest", false, FeatureStage.Experimental),
-  DISABLE_KOTLIN_ATTRIBUTE_SETUP("android.dependencyResolution.disable.kotlinPlatformTypeAttribute", false, FeatureStage.Experimental),
   /**
    * When enabled, incompatible APKs installed on a testing device will be uninstalled automatically during an instrumentation test run
    * (e.g. When INSTALL_FAILED_UPDATE_INCOMPATIBLE error happens after attempting to install APKs for testing).
@@ -773,7 +772,6 @@ enum class BooleanOption(
   /* ----------------
    * REMOVED FEATURES
    */
-
   @Suppress("unused") BUILD_FEATURE_BUILDCONFIG("android.defaults.buildfeatures.buildconfig", false, ApiStage.Removed(Version.VERSION_9_0)),
 
   /**
@@ -786,6 +784,11 @@ enum class BooleanOption(
     "android.enableLegacyVariantApi",
     false,
     ApiStage.Removed(Version.VERSION_9_0, "The android.enableLegacyVariantApi property has no effect, use android.newDsl instead"),
+  ),
+  DISABLE_KOTLIN_ATTRIBUTE_SETUP(
+    "android.dependencyResolution.disable.kotlinPlatformTypeAttribute",
+    false,
+    FeatureStage.Removed(Version.VERSION_9_2),
   ),
   @Suppress("unused")
   ENABLE_IN_PROCESS_AAPT2("android.enableAapt2jni", false, FeatureStage.Removed(VERSION_BEFORE_4_0, "AAPT2 JNI has been removed.")),
--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/app/AppWithKmpDependency.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/app/AppWithKmpDependency.kt
@@ -20,7 +20,6 @@ import com.android.build.gradle.integration.common.fixture.model.ModelComparator
 import com.android.build.gradle.integration.common.fixture.project.ApkSelector
 import com.android.build.gradle.integration.common.fixture.project.GradleRule
 import com.android.build.gradle.integration.common.fixture.project.builder.AndroidProjectDefinition.Companion.DEFAULT_APP_PATH
-import com.android.build.gradle.options.BooleanOption
 import com.android.builder.model.v2.ide.SyncIssue
 import org.junit.Rule
 import org.junit.Test
@@ -46,71 +45,10 @@ class AppWithKmpDependency : ModelComparator() {
   }
 
   @Test
-  fun `test VariantDependencies model without kotlin attribute`() {
-    val result =
-      rule.build.modelBuilder
-        .with(BooleanOption.DISABLE_KOTLIN_ATTRIBUTE_SETUP, true)
-        .with(BooleanOption.BUILT_IN_KOTLIN, false)
-        .ignoreSyncIssues(SyncIssue.SEVERITY_WARNING)
-        .fetchModels(variantName = "debug")
-
-    with(result)
-      .compareVariantDependencies(projectAction = { getProject(DEFAULT_APP_PATH) }, goldenFile = "app_VariantDependencies_desktop")
-  }
-
-  @Test
   fun checkPackagedClassesContainAndroidSpecificClass() {
     val build = rule.build
     build.executor.run(":app:assembleDebug")
 
     build.androidApplication().assertApk(ApkSelector.DEBUG) { classes().contains("androidx/lifecycle/ReportFragment") }
-  }
-
-  @Test
-  fun checkPackagedClassesDoesntContainAndroidSpecificClass() {
-    // should resolve desktopApiElements-published variant
-    val build = rule.build
-    build.executor
-      .with(BooleanOption.DISABLE_KOTLIN_ATTRIBUTE_SETUP, true)
-      .with(BooleanOption.BUILT_IN_KOTLIN, false)
-      .run(":app:assembleDebug")
-
-    build.androidApplication().assertApk(ApkSelector.DEBUG) {
-      // FIXME we should fix this by publishing a simple KMP library that only has a few classes
-      classes()
-        .subPackage("androidx/lifecycle")
-        .containsExactly(
-          "ClassesInfoCache$",
-          "CompositeGeneratedAdaptersObserver",
-          "DefaultLifecycleObserver$",
-          "DefaultLifecycleObserverAdapter$",
-          "DispatchQueue$",
-          "FlowExtKt$",
-          "GeneratedAdapter",
-          "GenericLifecycleObserver",
-          "Lifecycle$",
-          "LifecycleController$",
-          "LifecycleCoroutineScope$",
-          "LifecycleCoroutineScopeImpl$",
-          "LifecycleDestroyedException",
-          "LifecycleEventObserver",
-          "LifecycleKt$",
-          "LifecycleObserver",
-          "LifecycleOwner",
-          "LifecycleOwnerKt",
-          "LifecycleRegistry$",
-          "LifecycleRegistry_desktopKt",
-          "Lifecycle_jvmKt",
-          "Lifecycling",
-          "MethodCallsLogger",
-          "OnLifecycleEvent",
-          "PausingDispatcher",
-          "PausingDispatcherKt$",
-          "ReflectiveGenericLifecycleObserver",
-          "RepeatOnLifecycleKt$",
-          "SingleGeneratedAdapterObserver",
-          "WithLifecycleStateKt$",
-        )
-    }
   }
 }
