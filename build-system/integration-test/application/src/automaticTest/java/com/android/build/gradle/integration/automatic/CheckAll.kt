2708979d7e5b74cda81810bec8144c24271fd0f8
Luke Edgar <lukeedgar@google.com>
2026-03-11 14:31:09 +0000

    [10.0] Convert CheckAll.java to Kotlin
    Preparation for GradleRule migration
    
    Bug: n/a
    Test: n/a
    Change-Id: I425af8a9bdab410dd74eb2036a54e6b60ab3e597

--- build-system/integration-test/application/src/automaticTest/java/com/android/build/gradle/integration/automatic/CheckAll.kt
+++ build-system/integration-test/application/src/automaticTest/java/com/android/build/gradle/integration/automatic/CheckAll.kt
@@ -13,171 +13,148 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.build.gradle.integration.automatic
 
-package com.android.build.gradle.integration.automatic;
-
-import static com.google.common.base.Preconditions.checkState;
-
-import com.android.annotations.NonNull;
-import com.android.build.gradle.integration.common.fixture.BaseGradleExecutor.ConfigurationCaching;
-import com.android.build.gradle.integration.common.fixture.GradleTestProject;
-import com.android.build.gradle.integration.common.fixture.TestProjectPaths;
-import com.android.build.gradle.integration.common.runner.FilterableParameterized;
-import com.android.build.gradle.options.BooleanOption;
-import com.android.testutils.AssumeUtil;
-
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
-
-import org.junit.Assume;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
+import com.android.build.gradle.integration.common.fixture.BaseGradleExecutor.ConfigurationCaching
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.build.gradle.integration.common.fixture.GradleTestProject.Companion.builder
+import com.android.build.gradle.integration.common.fixture.TestProjectPaths
+import com.android.build.gradle.integration.common.runner.FilterableParameterized
+import com.android.build.gradle.options.BooleanOption
+import com.android.testutils.AssumeUtil
+import com.google.common.base.Preconditions
+import com.google.common.collect.ImmutableSet
+import com.google.common.collect.Lists
+import java.io.File
+import org.junit.Assume
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 /**
  * Test case that executes "standard" gradle tasks in all our tests projects.
  *
- * <p>You can run only one test like this:
+ * You can run only one test like this:
  *
- * <p>{@code ./gradlew :base:build-system:integration-test:application:automaticTest
- * --tests=*[abiPureSplits]}
+ * `./gradlew :base:build-system:integration-test:application:automaticTest --tests=*[abiPureSplits]`
  */
-@RunWith(FilterableParameterized.class)
-public class CheckAll {
+@RunWith(FilterableParameterized::class)
+class CheckAll(projectName: String, private val newDsl: Boolean) {
+  @Rule var project: GradleTestProject
 
+  @Test
+  @Throws(Exception::class)
+  fun assembleAndLint() {
+    AssumeUtil.assumeNotWindows() // b/73306170
+    Assume.assumeTrue(canAssemble(project))
+    if (newDsl) Assume.assumeFalse(requiresOldDsl(project))
+    project
+      .executor() // Test project depends on vector drawable libraries that violate unique
+      // namespacing.
+      .with(BooleanOption.ENFORCE_UNIQUE_PACKAGE_NAMES, false)
+      .with(BooleanOption.ENABLE_LEGACY_API, true)
+      .withEnableInfoLogging(false)
+      .run("assembleDebug", "assembleAndroidTest", "lint")
+  }
+
+  init {
+    project =
+      builder()
+        .fromTestProject(projectName)
+        .withConfigurationCaching(ConfigurationCaching.ON)
+        .withHeap("2048M")
+        .withComposeCompilerGradlePlugin(true)
+        .addGradleProperty(BooleanOption.USE_NEW_DSL, newDsl)
+        .create()
+  }
+
+  companion object {
     @Parameterized.Parameters(name = "{0}_newDsl_{1}")
-    public static Collection<Object[]> data() {
-        List<Object[]> parameters = Lists.newArrayList();
+    fun data(): MutableCollection<Array<Any?>?> {
+      val parameters: MutableList<Array<Any?>?> = Lists.newArrayList<Array<Any?>?>()
 
-        File[] testProjects = TestProjectPaths.getTestProjectDir().listFiles();
-        checkState(testProjects != null);
+      val testProjects = TestProjectPaths.getTestProjectDir().listFiles()
+      Preconditions.checkState(testProjects != null)
 
-        for (File testProject : testProjects) {
-            if (!isValidProjectDirectory(testProject)) {
-                continue;
-            }
-            for (Boolean newDsl : List.of(true, false)) {
-                parameters.add(new Object[]{testProject.getName(), newDsl});
-            }
+      for (testProject in testProjects!!) {
+        if (!isValidProjectDirectory(testProject)) {
+          continue
         }
-
-        return parameters;
-    }
-
-    private static boolean isValidProjectDirectory(File testProject) {
-        if (!testProject.isDirectory()) {
-            return false;
+        for (newDsl in mutableListOf<Boolean?>(true, false)) {
+          parameters.add(arrayOf(testProject.getName(), newDsl))
         }
-        // RenderScript support is removed after NDK r23 LTS
-        if (testProject.getName().equals("renderscriptNdk")) {
-            return false;
-        }
+      }
 
-        File buildGradle = new File(testProject, "build.gradle");
-        File settingsGradle = new File(testProject, "settings.gradle");
-
-        return buildGradle.exists() || settingsGradle.exists();
+      return parameters
     }
 
-    @Rule public GradleTestProject project;
+    private fun isValidProjectDirectory(testProject: File): Boolean {
+      if (!testProject.isDirectory) {
+        return false
+      }
+      // RenderScript support is removed after NDK r23 LTS
+      if (testProject.name == "renderscriptNdk") {
+        return false
+      }
 
-    private final Boolean newDsl;
+      val buildGradle = File(testProject, "build.gradle")
+      val settingsGradle = File(testProject, "settings.gradle")
 
-    public CheckAll(String projectName, Boolean newDsl) {
-        this.newDsl = newDsl;
-        project =
-                GradleTestProject.builder()
-                        .fromTestProject(projectName)
-                        .withConfigurationCaching(ConfigurationCaching.ON)
-                        .withHeap("2048M")
-                        .withComposeCompilerGradlePlugin(true)
-                        .addGradleProperty(BooleanOption.USE_NEW_DSL, newDsl)
-                        .create();
+      return buildGradle.exists() || settingsGradle.exists()
     }
 
-    @Test
-    public void assembleAndLint() throws Exception {
-        AssumeUtil.assumeNotWindows(); // b/73306170
-        Assume.assumeTrue(canAssemble(project));
-        if (newDsl) Assume.assumeFalse(requiresOldDsl(project));
-        project.executor()
-                // Test project depends on vector drawable libraries that violate unique
-                // namespacing.
-                .with(BooleanOption.ENFORCE_UNIQUE_PACKAGE_NAMES, false)
-                .with(BooleanOption.ENABLE_LEGACY_API, true)
-                .withEnableInfoLogging(false)
-                .run("assembleDebug", "assembleAndroidTest", "lint");
+    private fun canAssemble(project: GradleTestProject): Boolean {
+      return !BROKEN_ALWAYS_ASSEMBLE.contains(project.name)
     }
 
-    private static boolean canAssemble(@NonNull GradleTestProject project) {
-        return !BROKEN_ALWAYS_ASSEMBLE.contains(project.getName());
+    private fun requiresOldDsl(project: GradleTestProject): Boolean {
+      return OLD_DSL_PROJECTS.contains(project.name)
     }
 
-    private static boolean requiresOldDsl(@NonNull GradleTestProject project) {
-        return OLD_DSL_PROJECTS.contains(project.getName());
-    }
+    private val BROKEN_ALWAYS_ASSEMBLE: ImmutableSet<String?> =
+      ImmutableSet.of<String?>( // These require ndk.dir, but that's deprecated
+        "ndkJniLib",
+        "vulkan",
+        "ndkSanAngeles",
+        "combinedAbiDensitySplits", // Don't work with the version of the NDK that's in ndk-bundle. Enable after
+        // moving all test projects off ndk-bundle and onto ndk.
 
-    private static final ImmutableSet<String> BROKEN_ALWAYS_ASSEMBLE =
-            ImmutableSet.of(
-                    // These require ndk.dir, but that's deprecated
-                    "ndkJniLib",
-                    "vulkan",
-                    "ndkSanAngeles",
-                    "combinedAbiDensitySplits",
+        "prefabApp",
+        "prefabPublishing", // Data binding projects are tested in
+        // tools/base/build-system/integration-test/databinding
 
-                    // Don't work with the version of the NDK that's in ndk-bundle. Enable after
-                    // moving all test projects off ndk-bundle and onto ndk.
-                    "prefabApp",
-                    "prefabPublishing",
+        "databindingIncremental",
+        "databindingAndDagger",
+        "databinding",
+        "databindingAndKotlin",
+        "databindingAndJetifier",
+        "databindingMultiModule",
+        "databindingWithDynamicFeatures", // Requires ml models to be in place
+        "mlModelBinding", // These are all right:
+        "genFolderApi", // Has a required injectable property
+        "ndkJniPureSplitLib", // Doesn't build until externalNativeBuild {} is added.
+        "duplicateNameImport", // Fails on purpose.
+        "filteredOutBuildType", // assembleDebug does not exist as debug build type is
+        // removed.
+        "projectWithLocalDeps", // Doesn't have a build.gradle, not much to check
+        // anyway.
+        "externalBuildPlugin", // Not an Android Project.
+        "lintKotlin", // deliberately contains lint errors (missing baseline file)
+        "lintBaseline", // deliberately contains lint errors
+        "lintStandalone", // Not an Android project
+        "lintStandaloneVital", // Not an Android project
+        "lintStandaloneCustomRules", // Not an Android project
+        "lintCustomRules", // contains integ test for lint itself
+        "lintCustomLocalAndPublishRules", // contains integ test for lint itself
+        "simpleCompositeBuild", // broken composite build project.
+        "multiCompositeBuild", // too complex composite build project to setup
+        "sourceDependency", // not set up fully, just used for sync tests
+        "kotlinMultiplatform", // java multiplatform project has its own assemble
+        // tests.
+      )
 
-                    // Data binding projects are tested in
-                    // tools/base/build-system/integration-test/databinding
-                    "databindingIncremental",
-                    "databindingAndDagger",
-                    "databinding",
-                    "databindingAndKotlin",
-                    "databindingAndJetifier",
-                    "databindingMultiModule",
-                    "databindingWithDynamicFeatures",
-
-                    // Requires ml models to be in place
-                    "mlModelBinding",
-
-                    // These are all right:
-                    "genFolderApi", // Has a required injectable property
-                    "ndkJniPureSplitLib", // Doesn't build until externalNativeBuild {} is added.
-                    "duplicateNameImport", // Fails on purpose.
-                    "filteredOutBuildType", // assembleDebug does not exist as debug build type is
-                    // removed.
-                    "projectWithLocalDeps", // Doesn't have a build.gradle, not much to check
-                    // anyway.
-                    "externalBuildPlugin", // Not an Android Project.
-                    "lintKotlin", // deliberately contains lint errors (missing baseline file)
-                    "lintBaseline", // deliberately contains lint errors
-                    "lintStandalone", // Not an Android project
-                    "lintStandaloneVital", // Not an Android project
-                    "lintStandaloneCustomRules", // Not an Android project
-                    "lintCustomRules", // contains integ test for lint itself
-                    "lintCustomLocalAndPublishRules", // contains integ test for lint itself
-                    "simpleCompositeBuild", // broken composite build project.
-                    "multiCompositeBuild", // too complex composite build project to setup
-                    "sourceDependency", // not set up fully, just used for sync tests
-                    "kotlinMultiplatform" // java multiplatform project has its own assemble
-                    // tests.
-                    );
-
-    private static final ImmutableSet<String> OLD_DSL_PROJECTS =
-            ImmutableSet.of(
-                    "api",
-                    "artifactApi",
-                    "bytecodeGenerationHooksOld",
-                    "genFolderApi2Old",
-                    "renamedApk",
-                    "splitAwareSeparateTestModule"
-            );
+    private val OLD_DSL_PROJECTS: ImmutableSet<String?> =
+      ImmutableSet.of<String?>("api", "artifactApi", "bytecodeGenerationHooks", "renamedApk", "splitAwareSeparateTestModule")
+  }
 }
