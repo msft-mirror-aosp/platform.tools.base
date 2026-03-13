f49a79a177d340169b71c79242b1f87a60f46792
Esteban de la Canal <estebandlc@google.com>
2026-03-12 22:17:23 -0700

    Copy windows artifacts
    Test: NA
    Bug: NA
    Change-Id: I23728cd0ae0342cabdf50bbe7d5ca7017a5dae6e

--- bazel/ci/studio_win.py
+++ bazel/ci/studio_win.py
@@ -73,6 +73,8 @@ def studio_win(build_env: bazel.BuildEnv):
   studio.copy_artifacts(
       build_env,
       [
+          ('tools/vendor/google/android/android', ''),
+          ('tools/vendor/google/android/android-cli.zip', ''),
           ('tools/vendor/google/skia/skiaparser.zip', ''),
           ('tools/vendor/google/skia/skia_test_support.zip', ''),
           ('tools/base/profiler/native/trace_processor_daemon/trace_processor_daemon.exe', ''),
