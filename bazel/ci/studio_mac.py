79b0eb66d8d5019687d2f68199e04057f0ab8ac5
Esteban de la Canal <estebandlc@google.com>
2026-03-12 18:05:22 -0700

    Fix adnroid-cli mac artifacts
    Test: NA
    Bug: NA
    Change-Id: Ifc8731b0b038d5ec4b514085243b1b9dc4501061

--- bazel/ci/studio_mac.py
+++ bazel/ci/studio_mac.py
@@ -7,12 +7,11 @@ from tools.base.bazel.ci import bazel
 from tools.base.bazel.ci import studio
 from tools.base.bazel.ci.presubmit import impacted_targets
 
+# TODO: Why is this unused?
 _ARTIFACTS = [
     ('tools/adt/idea/studio/android-studio.linux.zip', 'artifacts'),
     ('tools/adt/idea/studio/android-studio.win.zip', 'artifacts'),
     ('tools/adt/idea/studio/android-studio.mac.zip', 'artifacts'),
-    ('tools/vendor/google/android/android', 'artifacts'),
-    ('tools/vendor/google/android/android-cli.zip', 'artifacts'),
 ]
 
 
@@ -52,6 +51,8 @@ def studio_mac(build_env: bazel.BuildEnv) -> None:
               ),
               ('tools/vendor/google/skia/skiaparser.zip', ''),
               ('tools/vendor/google/skia/skia_test_support.zip', ''),
+              ('tools/vendor/google/android/android', ''),
+              ('tools/vendor/google/android/android-cli.zip', ''),
           ],
       )
       studio.collect_logs(build_env, result.bes_path)
@@ -70,6 +71,8 @@ def studio_mac_arm(build_env: bazel.BuildEnv) -> None:
       build_env.bazel_query(*query).stdout.decode('utf-8').splitlines()
   )
   targets = test_targets + [
+      '//tools/vendor/google/android:android',
+      '//tools/vendor/google/android:android-cli.zip',
       '//tools/vendor/google/skia:skiaparser',
       '//tools/vendor/google/skia:skia_test_support',
       '//tools/base/profiler/native/trace_processor_daemon',
@@ -100,6 +103,8 @@ def studio_mac_arm(build_env: bazel.BuildEnv) -> None:
               ),
               ('tools/vendor/google/skia/skiaparser.zip', ''),
               ('tools/vendor/google/skia/skia_test_support.zip', ''),
+              ('tools/vendor/google/android/android', ''),
+              ('tools/vendor/google/android/android-cli.zip', ''),
           ],
       )
     if result.exit_code != bazel.EXITCODE_NO_TESTS_FOUND:
