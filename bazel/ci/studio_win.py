abf2216760c866b4eefe9280512975b5a0991150
Esteban de la Canal <estebandlc@google.com>
2026-03-17 18:07:12 -0700

    Fix windows artifact
    Test: this
    Bug: NA
    Change-Id: Ieca0043ae0bebcb17c88e05819b901f51bc300c5

--- bazel/ci/studio_win.py
+++ bazel/ci/studio_win.py
@@ -73,7 +73,7 @@ def studio_win(build_env: bazel.BuildEnv):
   studio.copy_artifacts(
       build_env,
       [
-          ('tools/vendor/google/android/android', ''),
+          ('tools/vendor/google/android/android.exe', ''),
           ('tools/vendor/google/android/android-cli.zip', ''),
           ('tools/vendor/google/skia/skiaparser.zip', ''),
           ('tools/vendor/google/skia/skia_test_support.zip', ''),
