442ce58fcd838589e5f52d1da9e2e9e90d3f7850
diegoperez <diegoperez@google.com>
2026-03-20 16:39:27 +0000

    Add AI Glasses display mode support to AVD config
    Introduce a new configuration setting for AI Glasses display modes in
    Android Virtual Devices. This allows the emulator to correctly
    simulate XR glasses hardware.
    
    The new `emulator.dev.xr.glasses_display` key supports
    `monocular_right` and `none` modes, with `monocular_right` as the
    default.
    
    Bug: 469422936
    Test: AdditionalSettingsPanelTest
    Change-Id: I269c0f73e5a6d77388d48cae678ee736e875ecc8

--- sdklib/src/main/java/com/android/sdklib/internal/avd/AvdBuilder.kt
+++ sdklib/src/main/java/com/android/sdklib/internal/avd/AvdBuilder.kt
@@ -95,6 +95,7 @@ class AvdBuilder(var metadataIniPath: Path, avdFolder: Path, var device: Device)
 
   var bootMode: BootMode = QuickBoot
 
+  var aiGlassesDisplayMode: AiGlassesDisplayMode = DEFAULT_AI_GLASSES_DISPLAY_MODE
   val userSettings = mutableMapOf<String, String>()
 
   val androidVersion: AndroidVersion?
@@ -164,6 +165,8 @@ class AvdBuilder(var metadataIniPath: Path, avdFolder: Path, var device: Device)
   }
 
   companion object {
+    val DEFAULT_AI_GLASSES_DISPLAY_MODE = AiGlassesDisplayMode.MONOCULAR_RIGHT
+
     /**
      * Values that we unconditionally set before setting other properties. They may be overridden by Device-determined properties. If they
      * need to be configurable, they can be converted to fields.
@@ -185,6 +188,7 @@ class AvdBuilder(var metadataIniPath: Path, avdFolder: Path, var device: Device)
         AvdBuilder::enableKeyboard bindToKey HardwareProperties.HW_KEYBOARD,
         AvdBuilder::networkLatency bindToKey ConfigKey.NETWORK_LATENCY,
         AvdBuilder::networkSpeed bindToKey ConfigKey.NETWORK_SPEED,
+        AvdBuilder::aiGlassesDisplayMode bindToKey ConfigKey.AI_GLASSES_DISPLAY_MODE,
       )
 
     /** Creates an AvdBuilder for editing an existing AVD. */
