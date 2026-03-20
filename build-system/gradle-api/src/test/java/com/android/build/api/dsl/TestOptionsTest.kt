d5f1d9dacec17c867d9295afba45e0d001a42f01
Alex Golubev <alexgolubev@google.com>
2026-03-20 12:24:14 -0400

    Hide unsupported declarative ExtensiblePolymorphicDomainObjectContainer
    Bug: NA
    Test: existing
    Change-Id: I2710dbb58d9953f4ebc15d36dc427ac3ff5eb52e

--- build-system/gradle-api/src/test/java/com/android/build/api/dsl/TestOptionsTest.kt
+++ build-system/gradle-api/src/test/java/com/android/build/api/dsl/TestOptionsTest.kt
@@ -21,6 +21,7 @@ import org.gradle.api.ExtensiblePolymorphicDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.DependencyCollector
 import org.gradle.api.provider.Provider
+import org.gradle.declarative.dsl.model.annotations.HiddenInDefinition
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.testing.base.TestSuite
 import org.junit.Before
@@ -50,6 +51,7 @@ class TestOptionsTest {
     override val targetVariants: MutableList<String>
       get() = throw RuntimeException("Unexpected call")
 
+    @HiddenInDefinition
     override fun getTargets(): ExtensiblePolymorphicDomainObjectContainer<AgpTestSuiteTarget> {
       throw RuntimeException("Unexpected call to `getTargets()`")
     }
