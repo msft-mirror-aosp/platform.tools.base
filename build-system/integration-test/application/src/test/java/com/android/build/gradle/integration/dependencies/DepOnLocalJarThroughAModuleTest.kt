479465ef5b2ec8018b3a66231042fdf656b643e3
Christophe Rhodes <xof@google.com>
2026-03-25 15:28:44 +0000

    Remove explicit disabling of built-in Kotlin in some tests
    This change does not in fact solve the underlying problem, because the
    test project itself ("projectWithModules") also disables built-in
    Kotlin.  It does, however, put us in a better place to enable built-in
    Kotlin in the project to see what breaks.
    
    Bug: none filed
    Test: test-only change
    Change-Id: I68ca2c4045d23c8a9569381f8ae4e065362961cf

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/DepOnLocalJarThroughAModuleTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/DepOnLocalJarThroughAModuleTest.kt
@@ -27,7 +27,7 @@ import org.junit.Test
 
 class DepOnLocalJarThroughAModuleTest : ModelComparator() {
 
-  @get:Rule val project = GradleTestProject.builder().fromTestProject("projectWithModules").disableBuiltInKotlin().create()
+  @get:Rule val project = GradleTestProject.builder().fromTestProject("projectWithModules").create()
 
   @Before
   fun setUp() {
