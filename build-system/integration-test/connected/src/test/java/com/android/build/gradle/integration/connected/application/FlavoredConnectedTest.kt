88a24b945af13c8518bb4aa1b46e24ddbee2a0b8
Yuki Hamada <hummer@google.com>
2026-04-01 14:30:05 -0700

    Migrate FlavoredConnectedTest to Kotlin and remove deprecated methods
    Migrate FlavoredConnectedTest from Java to Kotlin and remove the
    deprecated USE_NEW_DSL property and execute() method. This migration
    aligns the test with modern integration test practices in tools/base
    while maintaining functional equivalence with the original test.
    
    - Converted FlavoredConnectedTest.java to FlavoredConnectedTest.kt
    - Removed addGradleProperty(BooleanOption.USE_NEW_DSL, false)
    - Updated project.execute("uninstallAll") to project.executor().run("uninstallAll")
    - Applied ktfmt formatting
    
    Bug: None
    Test: FlavoredConnectedTest
    Change-Id: I38a9341ab01dbbc663ff7161643982050e74e2d6

---
+++ build-system/integration-test/connected/src/test/java/com/android/build/gradle/integration/connected/application/FlavoredConnectedTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.connected.application
+
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.build.gradle.integration.common.fixture.GradleTestProject.Companion.builder
+import com.android.build.gradle.integration.connected.utils.getEmulator
+import java.io.IOException
+import org.junit.Before
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExternalResource
+
+/** Connected tests for flavored. */
+class FlavoredConnectedTest {
+
+  @get:Rule val project: GradleTestProject = builder().fromTestProject("flavored").create()
+
+  @Before
+  @Throws(IOException::class)
+  fun setUp() {
+    // fail fast if no response
+    project.addAdbTimeout()
+    // run the uninstall tasks in order to (1) make sure nothing is installed at the beginning
+    // of each test and (2) check the adb connection before taking the time to build anything.
+    project.executor().run("uninstallAll")
+  }
+
+  @Test
+  @Throws(Exception::class)
+  fun connectedCheck() {
+    project.executor().run("connectedAndroidTest")
+  }
+
+  companion object {
+    @JvmField @ClassRule val emulator: ExternalResource = getEmulator()
+  }
+}
