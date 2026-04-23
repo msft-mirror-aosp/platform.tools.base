c743bf1f9e9042073c8bca15bc0f50dd06401d70
Paige Mcauliffe <paigemca@google.com>
2026-04-23 01:22:11 +0000

    Add styles so that links in lint issue explanation are highlighted
    The style is consistent with links in the more info section.
    
    Bug: 488042162
    Test: manually tested
    Change-Id: I591e8611163b982c5b57f70ff8b3da3a6a2fe318

--- lint/cli/src/main/java/com/android/tools/lint/renderer/LintReportStyle.kt
+++ lint/cli/src/main/java/com/android/tools/lint/renderer/LintReportStyle.kt
@@ -268,6 +268,8 @@ tr:hover { background-color: var(--bg-selected); }
 
 .explanation-row { background-color: var(--bg-subtle); }
 .explanation-content { padding: 1rem 2rem; white-space: normal; color: var(--text-gray-600); }
+.explanation-content a { color: var(--text-blue-600); }
+.explanation-content a:hover { text-decoration: underline; }
 .more-info-list { margin-left: 1.5rem; margin-top: 0.25rem; list-style-type: disc; }
 .more-info-list li { margin-bottom: 0.125rem; }
 
