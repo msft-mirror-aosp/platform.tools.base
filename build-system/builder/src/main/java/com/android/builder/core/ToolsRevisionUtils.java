f521c632bee0a3ce2d26d2d7f488cbe0ed954f95
Chris Warrington <cmw@google.com>
2026-05-05 22:48:21 +0100

    Consider 37.1 betas supported in AGP
    Fixes: 509813514
    Test: Manual
    Change-Id: I73cde4b0082c43a9b20ee1565ff072bf7d8516d6

--- build-system/builder/src/main/java/com/android/builder/core/ToolsRevisionUtils.java
+++ build-system/builder/src/main/java/com/android/builder/core/ToolsRevisionUtils.java
@@ -51,5 +51,5 @@ public class ToolsRevisionUtils {
      * encourage build authors to upgrade the Android Gradle Plugin.
      */
     public static final AndroidVersion MAX_RECOMMENDED_COMPILE_SDK_VERSION =
-            new AndroidVersion(37, 0);
+            new AndroidVersion(37, 1).withBetaNumber(Integer.MAX_VALUE);
 }
