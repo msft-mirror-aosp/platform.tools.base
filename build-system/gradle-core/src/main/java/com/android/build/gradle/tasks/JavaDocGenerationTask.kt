07e9d6a8e345656805fd868a3702ec8bb30aabc9
Bingran Li <bingran@google.com>
2026-05-20 13:34:49 -0700

    Revert "Enable parallelism for JavaDocGenerationTask"
    This reverts commit f4dc542b763c919ef2ca353ff70cf25e7ba73150.
    
    Reason for revert: flaky issue
    
    Bug: 211725171
    Change-Id: I2c9c0079c0bf87d4f70a696bb370713c75785387

--- build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/JavaDocGenerationTask.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/JavaDocGenerationTask.kt
@@ -18,6 +18,8 @@ package com.android.build.gradle.tasks
 
 import com.android.build.gradle.internal.component.ComponentCreationConfig
 import com.android.build.gradle.internal.scope.InternalArtifactType
+import com.android.build.gradle.internal.services.DokkaParallelBuildService
+import com.android.build.gradle.internal.services.getBuildService
 import com.android.build.gradle.internal.tasks.BuildAnalyzer
 import com.android.build.gradle.internal.tasks.NonIncrementalTask
 import com.android.build.gradle.internal.tasks.factory.VariantTaskCreationAction
@@ -206,6 +208,9 @@ abstract class JavaDocGenerationTask : NonIncrementalTask() {
 
     override fun configure(task: JavaDocGenerationTask) {
       super.configure(task)
+
+      val dokkaParallelBuildService = getBuildService(creationConfig.services.buildServiceRegistry, DokkaParallelBuildService::class.java)
+      task.usesService(dokkaParallelBuildService)
 
       task.moduleVersion.setDisallowChanges(task.project.version.toString())
 
