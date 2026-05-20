2b58fe0c24c1f8dc9250550c8dfb8a9d2fadf648
Madalin Valceleanu <vmadalin@google.com>
2026-05-20 10:26:19 +0000

    Update to KGP 2.4.0-RC for tests
    Bug: 503680018
    Test: Updated
    Change-Id: I4ee7623c93c9dd687d5857e8584f88e3ad0e4014

--- testutils/src/main/java/com/android/testutils/TestUtils.java
+++ testutils/src/main/java/com/android/testutils/TestUtils.java
@@ -61,7 +61,7 @@ public class TestUtils {
      * <p>This version needs to be present in prebuilts for tests to pass (see
      * tools/base/bazel/README.md).
      */
-    public static final String KOTLIN_VERSION_FOR_TESTS = "2.4.0-Beta2";
+    public static final String KOTLIN_VERSION_FOR_TESTS = "2.4.0-RC";
 
     /** KSP version used by AGP integration tests. */
     public static final String KSP_VERSION_FOR_TESTS = "2.2.20-RC-2.0.2";
