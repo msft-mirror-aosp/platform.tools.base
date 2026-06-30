47ce91ace5a16598619266b2611a32a3bf38fc43
Polina Koval <kovalp@google.com>
2026-06-30 17:49:43 +0100

    Add KMP AI Starter template and recipe
    Define a new "KMP AI Starter" template and its corresponding recipe that
    sets up a dual-module Kotlin Multiplatform project structure (app and
    shared). Register it in the default template provider.
    
    Implementation details:
    - Sets up the :shared module using com.android.kotlin.multiplatform.library.
    - Dynamically resolves version catalog reference keys for Kotlin and AGP
      versions instead of assuming static defaults.
    - Organizes the recipe components cleanly by placing build scripts,
      manifests, and source file templates under separate files in
      kmpAiStarter/src.
    - Properly escapes package names in template Kotlin files using
      escapeKotlinIdentifier to handle keywords.
    - Configures the :app module dynamically using merge-safe structured
      RecipeExecutor APIs (such as addDependency and addPlugin) to ensure robust
      Version Catalog generation, and setJavaKotlinCompileOptions(true) to
      safely establish JVM compiler options.
    - Merges AndroidManifest.xml safely using mergeXml to prevent conflicts with
      existing placeholders.
    - Defines the default @android:style/Theme.NoTitleBar theme directly in the
      manifest template to prevent resource-linking errors.
    
    Bug: 518799020
    Test: com.android.studio.ml.entry.ImportProjectEntryTest
    Change-Id: Id8aa134a755fe706b4904893d7a232c241cf5738

---
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/kmpAiStarter/kmpAiStarterTemplate.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.wizard.template.impl.activities.kmpAiStarter
+
+import com.android.tools.idea.wizard.template.Category
+import com.android.tools.idea.wizard.template.CheckBoxWidget
+import com.android.tools.idea.wizard.template.Constraint.CLASS
+import com.android.tools.idea.wizard.template.Constraint.NONEMPTY
+import com.android.tools.idea.wizard.template.Constraint.UNIQUE
+import com.android.tools.idea.wizard.template.FormFactor
+import com.android.tools.idea.wizard.template.LanguageWidget
+import com.android.tools.idea.wizard.template.ModuleTemplateData
+import com.android.tools.idea.wizard.template.PackageNameWidget
+import com.android.tools.idea.wizard.template.TemplateConstraint
+import com.android.tools.idea.wizard.template.TemplateData
+import com.android.tools.idea.wizard.template.TemplateFlag
+import com.android.tools.idea.wizard.template.TextFieldWidget
+import com.android.tools.idea.wizard.template.WizardUiContext
+import com.android.tools.idea.wizard.template.booleanParameter
+import com.android.tools.idea.wizard.template.impl.defaultPackageNameParameter
+import com.android.tools.idea.wizard.template.stringParameter
+import com.android.tools.idea.wizard.template.template
+import java.io.File
+
+val kmpAiStarterTemplate
+  get() = template {
+    name = "KMP AI Starter"
+    description = "Create a new KMP app using AI"
+    minApi = 21
+    constraints = listOf(TemplateConstraint.AndroidX, TemplateConstraint.Kotlin, TemplateConstraint.Material3, TemplateConstraint.Compose)
+    category = Category.Application
+    flags = listOf(TemplateFlag.NewProjectAgent)
+    formFactor = FormFactor.Mobile
+    screens = listOfNotNull(WizardUiContext.NewProject)
+
+    val activityClass = stringParameter {
+      name = "Activity Name"
+      default = "MainActivity"
+      help = "The name of the activity class to create"
+      constraints = listOf(CLASS, UNIQUE, NONEMPTY)
+      loggable = true
+    }
+
+    val packageName = defaultPackageNameParameter
+
+    val isLauncher = booleanParameter {
+      name = "Launcher Activity"
+      default = true
+      help = "If true, this activity will have a CATEGORY_LAUNCHER intent filter, making it visible in the launcher"
+    }
+
+    widgets(TextFieldWidget(activityClass), PackageNameWidget(packageName), CheckBoxWidget(isLauncher), LanguageWidget())
+
+    thumb { File("compose-activity-material3").resolve("template_compose_empty_activity_material3.png") }
+
+    recipe = { data: TemplateData ->
+      kmpAiStarterRecipe(data as ModuleTemplateData, activityClass.value, packageName.value, isLauncher.value)
+    }
+  }
---
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/kmpAiStarter/sharedBuildGradle.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.wizard.template.impl.activities.kmpAiStarter
+
+fun sharedBuildGradle(packageName: String, compileSdk: Int, minSdk: Int) =
+  """
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "$packageName.shared"
+        compileSdk = $compileSdk
+        minSdk = $minSdk
+
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+            }
+        }
+        commonTest {
+            dependencies {
+            }
+        }
+    }
+}
+"""
+    .trimIndent()
