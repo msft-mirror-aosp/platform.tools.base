c2815e570a14340f54d70d47aeb936356cafbf15
Alan Leung <acleung@google.com>
2026-06-30 15:55:21 -0700

    Revert "Make sure Properties are available"
    This reverts commit dc644ec04a275fbbd101146845c1b3ea60128a74.
    
    Reason for revert: Potentially made it MORE flakey.
    
    Change-Id: I7e531c1de4c8b43765e0783343cd3dd96cd817a2

--- deploy/deployer/src/test/java/com/android/tools/deployer/ApkVerifierTrackerTest.java
+++ deploy/deployer/src/test/java/com/android/tools/deployer/ApkVerifierTrackerTest.java
@@ -114,11 +114,6 @@ public class ApkVerifierTrackerTest {
             deviceStates.add(connectAndWaitForDevice(fakeDevice));
         }
 
-        // Make sure all properties are fetched.
-        for (IDevice d : bridge.getDevices()) {
-            d.getSystemProperty(IDevice.PROP_BUILD_API_LEVEL).get(50, TimeUnit.MILLISECONDS);
-        }
-
         Map<FakeDevice, IDevice> devicesMap = new HashMap<>();
 
         // Map FakeDevices to their corresponding IDevices.
