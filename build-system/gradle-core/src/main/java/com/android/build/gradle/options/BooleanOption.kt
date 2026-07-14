338d532f43e3106f555d9867ff068f1175501988
Yuki Hamada <hummer@google.com>
2026-07-13 18:15:29 -0700

    Revert "Enable Android Test JUnit Engine by default"
    Revert submission 39741294-enable_android_test_engine
    
    Reason for revert: Using AbstractTestTask for androidTest should be considered as API change and wait for AGP 10
    
    Reverted changes: /q/submissionid:39741294-enable_android_test_engine
    
    Bug: 432544694
    Change-Id: I8aa20c314cabec8c220290c9d27b23a9a37a1ab0

--- build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
@@ -332,10 +332,10 @@ enum class BooleanOption(
     FutureStage(Version.VERSION_9_0, true, FeatureStage.Enforced(Version.VERSION_9_0)),
   ),
 
-  /** Switches Android Test execution from the Unified Test Platform (UTP) to the Android Test JUnit Engine. */
+  /** Switches Android Test execution from the Unified Test Platform (UTP) to the experimental direct-to-AGP implementation. */
   ANDROID_BUILTIN_TEST_PLATFORM(
     "android.experimental.androidTest.builtin_test_platform",
-    true,
+    false,
     FeatureStage.Experimental,
     futureStage = FutureStage(defaultValue = true, stage = FeatureStage.SoftlyEnforced(VERSION_10_0), version = Version.VERSION_9_0),
   ),
