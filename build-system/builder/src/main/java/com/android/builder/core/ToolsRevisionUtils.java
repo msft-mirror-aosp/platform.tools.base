ee608adcef7bf9566bc2adbf217020ecb2f544fe
Luke Edgar <lukeedgar@google.com>
2026-07-30 18:38:20 +0100

    [java res] Consider 37.2 as supported in AGP
    Fixes: 538558858
    Test: Manual
    Change-Id: I9f3cb70461efed2efc2a2a782b494679cd56946c

--- build-system/builder/src/main/java/com/android/builder/core/ToolsRevisionUtils.java
+++ build-system/builder/src/main/java/com/android/builder/core/ToolsRevisionUtils.java
@@ -51,5 +51,5 @@ public class ToolsRevisionUtils {
      * encourage build authors to upgrade the Android Gradle Plugin.
      */
     public static final AndroidVersion MAX_RECOMMENDED_COMPILE_SDK_VERSION =
-            new AndroidVersion(37, 1);
+            new AndroidVersion(37, 2);
 }
