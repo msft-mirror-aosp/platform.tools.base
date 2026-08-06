1778c23651328bd2f16f902867e7ceee11dba3f3
James Rose <rosej@google.com>
2026-08-06 12:44:25 -0700

    Convert UpdatablePackage to Kotlin
    Also, make it immutable.
    
    Bug: n/a
    Test: n/a
    Change-Id: I4bda22ca9dd2f4a426578c693145d939655862c3

--- repository/src/main/java/com/android/repository/api/UpdatablePackage.kt
+++ repository/src/main/java/com/android/repository/api/UpdatablePackage.kt
@@ -13,102 +13,46 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package com.android.repository.api;
-
-import com.android.annotations.NonNull;
-import com.android.annotations.Nullable;
+package com.android.repository.api
 
 /**
- * Represents a (revisionless) package, either local, remote, or both. If both a local and remote
- * package are specified, they should represent exactly the same package, excepting the revision.
- * That is, the result of installing the remote package should be (a possibly updated version of)
- * the local package.
+ * Represents a (revisionless) package, either local, remote, or both. If both a local and remote package are specified, they should
+ * represent exactly the same package, excepting the revision. That is, the result of installing the remote package should be (a possibly
+ * updated version of) the local package.
  */
-public class UpdatablePackage implements Comparable<UpdatablePackage> {
+class UpdatablePackage(val local: LocalPackage?, val remote: RemotePackage?) : Comparable<UpdatablePackage> {
 
-    private LocalPackage mLocalPackage;
+  constructor(local: LocalPackage) : this(local, null)
 
-    private RemotePackage mRemotePackage;
+  constructor(remote: RemotePackage) : this(null, remote)
 
-    public UpdatablePackage(@NonNull LocalPackage localPackage) {
-        init(localPackage, null);
+  init {
+    require(local != null || remote != null) { "At least one of local or remote package must be specified." }
+    require(local == null || remote == null || local.path == remote.path) { "Local and remote paths cannot be different." }
+  }
+
+  fun hasRemote(): Boolean = remote != null
+
+  fun hasLocal(): Boolean = local != null
+
+  override fun compareTo(other: UpdatablePackage): Int = representative.compareTo(other.representative)
+
+  /**
+   * Gets a [RepoPackage] (either local or remote) corresponding to this updatable package. This will be the local package if there is one,
+   * and the remote otherwise.
+   */
+  val representative: RepoPackage
+    get() = local ?: remote ?: error("UpdatablePackage must have a local or remote package")
+
+  /** Indicates that this package is installed and a newer version is available. */
+  val isUpdate: Boolean
+    get() {
+      val local = local ?: return false
+      val remote = remote ?: return false
+      return local.version < remote.version
     }
 
-    public UpdatablePackage(@NonNull RemotePackage remotePackage) {
-        init(null, remotePackage);
-    }
-
-    public UpdatablePackage(@NonNull LocalPackage localPackage,
-            @NonNull RemotePackage remotePackage) {
-        init(localPackage, remotePackage);
-    }
-
-    private void init(@Nullable LocalPackage localPkg, @Nullable RemotePackage remotePkg) {
-        assert localPkg != null || remotePkg != null;
-        mLocalPackage = localPkg;
-        if (remotePkg != null) {
-            setRemote(remotePkg);
-        }
-    }
-
-    /**
-     * Sets the remote package for this {@code UpdatablePackage}.
-     */
-    public void setRemote(@NonNull RemotePackage remote) {
-        mRemotePackage = remote;
-    }
-
-    @Nullable
-    public LocalPackage getLocal() {
-        return mLocalPackage;
-    }
-
-    @Nullable
-    public RemotePackage getRemote() {
-        return mRemotePackage;
-    }
-
-    public boolean hasRemote() {
-        return getRemote() != null;
-    }
-
-    public boolean hasLocal() {
-        return mLocalPackage != null;
-    }
-
-    @Override
-    public int compareTo(@NonNull UpdatablePackage o) {
-        return getRepresentative().compareTo(o.getRepresentative());
-    }
-
-    /**
-     * Gets a {@link RepoPackage} (either local or remote) corresponding to this updatable package.
-     * This will be the local package if there is one, and the remote otherwise.
-     */
-    @NonNull
-    public RepoPackage getRepresentative() {
-        if (hasLocal()) {
-            return mLocalPackage;
-        }
-        // getRemote() must be non-null if there's no local
-        //noinspection ConstantConditions
-        return getRemote();
-    }
-
-    /**
-     * @return {@code true} if this package is installed and a newer version is available.
-     */
-    public boolean isUpdate() {
-        RemotePackage remote = getRemote();
-        return mLocalPackage != null && remote != null
-                && mLocalPackage.getVersion().compareTo(remote.getVersion()) < 0;
-    }
-
-    /**
-     * @return The {@link RepoPackage#getPath() path} of the local and/or remote package.
-     */
-    public String getPath() {
-        return getRepresentative().getPath();
-    }
+  /** The [path][RepoPackage.getPath] of the local and/or remote package. */
+  val path: String
+    get() = representative.path
 }
