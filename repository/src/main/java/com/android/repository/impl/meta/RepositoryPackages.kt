d76b91b4ff515ffb336bd3e0bed69f42845e9178
James Rose <rosej@google.com>
2026-08-06 10:43:47 -0700

    Stop caching new / updated packages
    These are only used in the sdkmanager command-line tool, and easy to
    compute dynamically.
    
    Bug: n/a
    Test: RepositoryPackagesTest
    Change-Id: I870213b11dd17f8ac8335680f1d9dbaae8f2eeca

--- repository/src/main/java/com/android/repository/impl/meta/RepositoryPackages.kt
+++ repository/src/main/java/com/android/repository/impl/meta/RepositoryPackages.kt
@@ -41,16 +41,10 @@ class RepositoryPackages() {
   var remotePackages: ImmutableSortedMap<String, RemotePackage> = ImmutableSortedMap.of()
     private set
 
-  /** All the packages that are locally-installed and have a remotely-available update. */
-  private var mUpdatedPkgs: MutableSet<UpdatablePackage>? = sortedSetOf()
-
-  /** All the packages that are available remotely and don't have an installed version. */
-  private var mNewPkgs: MutableSet<RemotePackage>? = sortedSetOf()
-
   /** Map from `path` (the unique ID of a package) to [UpdatablePackage], including all packages installed or available. */
-  private var mConsolidatedPkgs: MutableMap<String, UpdatablePackage>? = TreeMap()
+  private var _consolidatedPkgs: MutableMap<String, UpdatablePackage>? = TreeMap()
 
-  private val mLock = Any()
+  private val lock = Any()
 
   /**
    * Returns the set of packages that have local updates available.
@@ -58,14 +52,7 @@ class RepositoryPackages() {
    * @return A non-null, possibly empty Set of update candidates.
    */
   val updatedPkgs: Set<UpdatablePackage>
-    get() {
-      synchronized(mLock) {
-        if (mUpdatedPkgs == null) {
-          computeUpdates()
-        }
-        return mUpdatedPkgs!!
-      }
-    }
+    get() = consolidatedPkgs.values.filterTo(HashSet()) { it.isUpdate }
 
   /**
    * Returns the set of new remote packages that are not locally present and that the user could install.
@@ -73,27 +60,19 @@ class RepositoryPackages() {
    * @return A non-null, possibly empty Set of new install candidates.
    */
   val newPkgs: Set<RemotePackage>
-    get() {
-      synchronized(mLock) {
-        if (mNewPkgs == null) {
-          computeUpdates()
-        }
-        return mNewPkgs!!
-      }
-    }
+    get() = consolidatedPkgs.values.filter { !it.hasLocal() }.mapTo(HashSet()) { it.remote!! }
 
   /**
    * Returns a map of package install ids to [UpdatablePackage]s representing all known local and remote packages. Remote packages
-   * corresponding to local packages will be represented by a single item containing both the local and remote info. {@see *
-   * IPkgDesc#getInstallId()}
+   * corresponding to local packages will be represented by a single item containing both the local and remote info.
    */
   val consolidatedPkgs: Map<String, UpdatablePackage>
     get() {
-      synchronized(mLock) {
-        if (mConsolidatedPkgs == null) {
-          computeUpdates()
+      synchronized(lock) {
+        _consolidatedPkgs?.let {
+          return it
         }
-        return mConsolidatedPkgs!!
+        return computeUpdates().also { _consolidatedPkgs = it }
       }
     }
 
@@ -109,7 +88,7 @@ class RepositoryPackages() {
    * Sets the collection of known [LocalPackage]s, and recomputes the list of updates and new packages, if [RemotePackage]s have been set.
    */
   fun setLocalPkgInfos(packages: Collection<LocalPackage>) {
-    synchronized(mLock) {
+    synchronized(lock) {
       this.localPackages = ImmutableSortedMap.copyOf(packages.associateBy { it.path })
       invalidate()
     }
@@ -119,44 +98,31 @@ class RepositoryPackages() {
    * Sets the collection of known [RemotePackage]s, and recomputes the list of updates and new packages, if [LocalPackage]s have been set.
    */
   fun setRemotePkgInfos(packages: Collection<RemotePackage>) {
-    synchronized(mLock) {
+    synchronized(lock) {
       this.remotePackages = ImmutableSortedMap.copyOf(packages.associateBy { it.path })
       invalidate()
     }
   }
 
   private fun invalidate() {
-    mConsolidatedPkgs = null
-    mNewPkgs = null
-    mUpdatedPkgs = null
+    _consolidatedPkgs = null
   }
 
-  private fun computeUpdates() {
-    val newConsolidatedPkgs = TreeMap<String, UpdatablePackage>()
-    val updates = mutableSetOf<UpdatablePackage>()
+  private fun computeUpdates(): TreeMap<String, UpdatablePackage> {
+    val consolidatedPkgs = TreeMap<String, UpdatablePackage>()
 
-    for ((path, local) in localPackages) {
-      val updatable = UpdatablePackage(local)
-      newConsolidatedPkgs[path] = updatable
-      remotePackages[path]?.let { remote ->
-        updatable.setRemote(remote)
-        if (updatable.isUpdate) {
-          updates.add(updatable)
+    for (path in localPackages.keys + remotePackages.keys) {
+      val local = localPackages[path]
+      val remote = remotePackages[path]
+
+      consolidatedPkgs[path] =
+        when {
+          local != null && remote != null -> UpdatablePackage(local, remote)
+          local != null -> UpdatablePackage(local)
+          remote != null -> UpdatablePackage(remote)
+          else -> throw IllegalStateException() // not possible
         }
-      }
     }
-
-    val news = mutableSetOf<RemotePackage>()
-    for ((path, remote) in remotePackages) {
-      if (!newConsolidatedPkgs.containsKey(path)) {
-        news.add(remote)
-        val updatable = UpdatablePackage(remote)
-        newConsolidatedPkgs[path] = updatable
-      }
-    }
-
-    mNewPkgs = news
-    mUpdatedPkgs = updates
-    mConsolidatedPkgs = newConsolidatedPkgs
+    return consolidatedPkgs
   }
 }
