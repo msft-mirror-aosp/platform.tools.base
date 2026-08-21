5f9df61365f021822a3f925dd7782ed317678f52
samaypatel <samaypatel@google.com>
2026-08-21 06:56:21 +0000

    Add ignored paths for mutation testing
    Exclude test utilities, evals, and generated directories from mutation testing in config.py.
    
    Bug: 542837222
    Test: n/a
    Change-Id: Ib6ae5e932b793eb945c6219a0e870b89072da1ae

--- bazel/mutation/config.py
+++ bazel/mutation/config.py
@@ -12,4 +12,13 @@ IGNORE_PATHS = [
     re.compile(r"(?i).*/test/.*"),
     re.compile(r"Test\.(kt|java)$"),
     re.compile(r".*/(testSrc|testData)/.*"),
+    "tools/adt/idea/ij-debugger-tests",
+    "tools/vendor/google/ml/evals",
+    "tools/vendor/google/ml/aiplugin/ij-platform/src/main/kotlin/com/google/tools/intellij/aiplugin/feedback",
+    "tools/base/jdwp-packet",
+    "tools/base/jdwp-tracer",
+    "tools/base/multipreview-asm/src/perfTest",
+    "tools/base/dynamic-layout-inspector/agent/appinspection/fake-android",
+    "tools/adt/idea/designer/testFramework",
+    "tools/adt/idea/wear-dwf/gen",
 ]
