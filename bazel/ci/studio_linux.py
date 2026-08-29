028c390ee281324d581726d86984343c7d4a4733
Esteban de la Canal <estebandlc@google.com>
2026-08-29 11:52:48 -0700

    Copy Android CLI docs to artifacts
    Test: Not needed
    Bug: NA
    Change-Id: If9917eb5f567fb06542436226dddd84c04bec41d

--- bazel/ci/studio_linux.py
+++ bazel/ci/studio_linux.py
@@ -133,6 +133,7 @@ _ARTIFACTS = [
     ('tools/base/apkparser/analyzer/libtools.analyzer.jar', 'artifacts'),
     ('prebuilts/studio/layoutlib/layoutlib-repository.zip', 'artifacts'),
     ('tools/vendor/google/android/android-cli.zip', 'artifacts'),
+    ('tools/vendor/google/android/android-cli-docs.zip', 'artifacts'),
     ('tools/vendor/google/android/create/dist/templates-sdk-package.zip', 'artifacts'),
     ('tools/vendor/google/android/dist/deb/android-cli.deb.zip', 'artifacts'),
     ('tools/vendor/google/android/dist/deb/android-cli.deb', 'artifacts'),
