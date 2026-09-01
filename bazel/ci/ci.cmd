79463e7e2cb54de4de3be091f96a8e5024f0a090
Shane Almeida <shanealmeida@google.com>
2026-09-01 10:19:15 -0400

    Increase the timeout for the credential helper.
    Builds occasionally fail due to timeouts with the credential helper.
    Logging added in http://ag/41686537 does not appear when the timeout
    happens.  This suggests that calling the helper may be what is actually
    timing out, not the helper itself.
    
    The default is 10s [1].  Increasing to 60s seems like a reasonable
    place to start.
    
    There are other configs where the credential helper is used [2], but I
    started with just the main Linux, Mac, and Windows CI configs for now.
    
    1. https://github.com/bazelbuild/bazel/blob/3651243fd2025d0c4efb96a11d24d6f4e729b531/src/main/java/com/google/devtools/build/lib/authandtls/AuthAndTLSOptions.java#L250
    2. https://source.corp.google.com/h/googleplex-android/platform/superproject/base/+/studio-main:tools/base/bazel/studio_coverage.sh;l=47-48;drc=ec2cfafd95405c8d9d43f2545f1366b6acb6a2ec,
       https://source.corp.google.com/h/googleplex-android/platform/superproject/base/+/studio-main:tools/base/bazel/perfgate_win.cmd;l=31-32;drc=ec2cfafd95405c8d9d43f2545f1366b6acb6a2ec,
       https://source.corp.google.com/h/googleplex-android/platform/superproject/base/+/studio-main:tools/base/bazel/perfgate_linux.sh;l=28-29;drc=acdfc8cec5b659e76a1cbdbadafbe9441925d43c,
       https://source.corp.google.com/h/googleplex-android/platform/superproject/base/+/studio-main:tools/base/bazel/ci/mutation_testing.sh;l=37-38;drc=ff84bf63a9abec4ed4ccac2b23deaa4d65187de3
    
    Bug: 554045956
    Test: N/A
    Change-Id: I2c6a6681eeb8a4fa8a0778b3e78a78c7575cc294

--- bazel/ci/ci.cmd
+++ bazel/ci/ci.cmd
@@ -10,6 +10,7 @@ set ANDROID_HOME=$WORKSPACE_ROOT\prebuilts\studio\sdk\windows
 if defined BUILD_NUMBER (
   echo common --credential_helper=*.pkg.dev=%%workspace%%/build/bazel/tools/ci_credhelper.cmd > ci.bazelrc
   echo common --credential_helper=*.googleapis.com=%%workspace%%/build/bazel/tools/ci_credhelper.cmd >> ci.bazelrc
+  echo common --credential_helper_timeout=60s >> ci.bazelrc
 )
 
 %BAZEL% --max_idle_secs=60 run --config=ci --config=remote-exec //tools/base/bazel/ci -- %1
