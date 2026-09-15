ec3ea5d4b92da6a8c0a04d1b71dad2e874d60f52
Daniel Rosenberg <drosen@google.com>
2026-09-15 15:55:20 -0700

    Change XR Dimming Levls to go from 0.2 to 0.95
    Values are rounded to the nearest 1/256th to avoid some poor float
    rounding behaviors from the guest image.
    
    Bug: 555354162
    Test: Booted, Checked UI behavior on Embedded + Standalone
    Change-Id: I0e1798a5b1f570b28d5f3b84f0e006f350081035

--- sdklib/src/main/java/com/android/sdklib/internal/avd/HardwareProperties.java
+++ sdklib/src/main/java/com/android/sdklib/internal/avd/HardwareProperties.java
@@ -400,7 +400,7 @@ public class HardwareProperties {
             props.put(ConfigKey.LCD_TRANSPARENT, "yes");
             props.put(HW_LED_INDICATORS, "yes");
         } else if (isXrGlasses(d)) {
-            props.put(HW_DIMMING_LEVELS, "0.00390625,0.25,0.5,0.75,0.99609375");
+            props.put(HW_DIMMING_LEVELS, "0.19921875,0.38671875,0.57421875,0.76171875,0.94921875");
         }
 
         HashFunction md5 = Hashing.md5();
